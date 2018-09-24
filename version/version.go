2ca70f0b4cef940094575667bc79b6b89d99c1c0
Shweini <code.maestro64@gmail.com>
2018-09-24 22:20:05 +0100

    Add utils that connects to and calls services on dcrwallet grpc server

---
+++ version/version.go
@@ -0,0 +1,35 @@
+package version
+
+import "fmt"
+
+type Version struct {
+	Major, Minor, Patch int
+	Label               string
+	Nick                string
+}
+
+var Ver = Version{
+	Major: 0,
+	Minor: 0,
+	Patch: 1,
+	Label: "",
+}
+
+// CommitHash may be set on the build command line:
+// go build -ldflags "-X github.com/decred/dcrdata/version.CommitHash=`git describe --abbrev=8 --long | awk -F "-" '{print $(NF-1)"-"$NF}'`"
+var CommitHash string
+
+const AppName string = "dcrcli"
+
+func (v *Version) String() string {
+	var hashStr string
+	if CommitHash != "" {
+		hashStr = "+" + CommitHash
+	}
+	if v.Label != "" {
+		return fmt.Sprintf("%d.%d.%d-%s%s",
+			v.Major, v.Minor, v.Patch, v.Label, hashStr)
+	}
+	return fmt.Sprintf("%d.%d.%d%s",
+		v.Major, v.Minor, v.Patch, hashStr)
+}
