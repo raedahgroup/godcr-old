4511a763efa06fc7ab3608ee13c62e0ce864f191
Jonathanks <barry.jonathanks@gmail.com>
2018-12-04 15:33:33 +0100

    Merge flags `httpserveraddress` and `mode` into one (`http`).
    The program runs in CLI mode by default. If `--http` flag is set, it
    should switch to HTTP mode. Using `-http` to specify the address is
    inspired by godoc's --http flag.
    This change requires changes to the config file format: the `mode`
    setting shall be removed, and the `httpserveraddress` setting shall be
    renamed to `http`.

--- config.go
+++ config.go
@@ -34,9 +34,8 @@ type config struct {
 	RPCPassword       string `short:"P" long:"rpcpass" default-mask:"-" description:"RPC password"`
 	WalletRPCServer   string `short:"w" long:"walletrpcserver" description:"Wallet RPC server to connect to"`
 	RPCCert           string `short:"c" long:"rpccert" description:"RPC server certificate chain for validation"`
-	HTTPServerAddress string `short:"p" long:"httpserveraddress" description:"Http address to serve if mode is set to http"`
+	HTTPServerAddress string `long:"http" description:"HTTP address to serve. This turns HTTP mode on."`
 	NoDaemonTLS       bool   `long:"nodaemontls" description:"Disable TLS"`
-	Mode              string `short:"m" long:"mode" description:"Toggles between 'http' and 'cli' modes"`
 }
 
 func cleanAndExpandPath(path string) string {
