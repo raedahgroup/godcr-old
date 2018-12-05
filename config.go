0c272a35dbcebe058bcfee1405474e1856b06f1e
Jonathanks <barry.jonathanks@gmail.com>
2018-12-05 15:37:41 +0100

    Separate HTTP server address key from HTTP mode key.
    --http is a boolean flag: no value can be passed with it.
    Its presence switches the program to HTTP mode.

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ type config struct {
 	RPCPassword       string `short:"P" long:"rpcpass" default-mask:"-" description:"RPC password"`
 	WalletRPCServer   string `short:"w" long:"walletrpcserver" description:"Wallet RPC server to connect to"`
 	RPCCert           string `short:"c" long:"rpccert" description:"RPC server certificate chain for validation"`
-	HTTPServerAddress string `long:"httpserveraddress" description:"address and port of the HTTP server."`
+	HTTPServerAddress string `long:"httpserveraddress" description:"HTTP address to serve. This turns HTTP mode on."`
 	HTTPMode          bool   `long:"http" description:"run in HTTP mode. Requires httpserveraddress to be set"`
 	NoDaemonTLS       bool   `long:"nodaemontls" description:"Disable TLS"`
 }
