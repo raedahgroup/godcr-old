ac2224c6abfdc341abf1f68c26f9f006da326ebb
Jonathanks <barry.jonathanks@gmail.com>
2018-12-19 20:47:41 +0100

    Deleted unused code.

--- cli/cli.go
+++ cli/cli.go
@@ -6,31 +6,17 @@ import (
 	"github.com/raedahgroup/dcrcli/walletrpcclient"
 )
 
-type (
-	response struct {
-		columns []string
-		result  [][]interface{}
-	}
-	// handler carries out the action required by a command.
-	// commandArgs holds the arguments passed to the command.
-	handler func(walletrpcclient *walletrpcclient.Client, commandArgs []string) (*response, error)
-
-	// cli holds data needed to run the program.
-	cli struct {
-		funcMap         map[string]handler
-		appName         string
-		walletrpcclient *walletrpcclient.Client
-	}
-)
+type response struct {
+	columns []string
+	result  [][]interface{}
+}
 
 var (
-	walletClient    *walletrpcclient.Client
-	applicationName string
-	stdoutWriter    = tabWriter(os.Stdout)
+	walletClient *walletrpcclient.Client
+	stdoutWriter = tabWriter(os.Stdout)
 )
 
 // Setup initializes the states of variables used by this package.
-func Setup(walletrpcclient *walletrpcclient.Client, appName string) {
+func Setup(walletrpcclient *walletrpcclient.Client) {
 	walletClient = walletrpcclient
-	applicationName = appName
 }
