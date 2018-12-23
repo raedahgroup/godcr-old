1c4a21825c53ed11d7be38f574db84002c94cbe6
Jonathanks <barry.jonathanks@gmail.com>
2018-12-23 11:39:04 +0100

    Handle --help and -v flags, ignore commands during config parsing.

--- cli/app.go
+++ cli/app.go
@@ -1,7 +1,11 @@
 package cli
 
+import (
+	"github.com/raedahgroup/dcrcli/cli/core"
+)
+
 type AppCommands struct {
-	Config
+	core.Config
 	Balance    BalanceCommand    `command:"balance" description:"show your balance"`
 	Send       SendCommand       `command:"send" description:"send a transaction" subcommands-optional:"optional"`
 	SendCustom SendCustomCommand `command:"send-custom" description:"send a transaction, manually selecting inputs from unspent outputs"`
