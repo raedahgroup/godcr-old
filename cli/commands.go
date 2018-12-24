ca4969e02f5f9e1bc3260551484492197d6386e4
Jonathanks <barry.jonathanks@gmail.com>
2018-12-24 15:42:34 +0100

    Move config out of cli package, rename from core to config. Move AppCommands to commands.go

--- cli/commands.go
+++ cli/commands.go
@@ -1,5 +1,16 @@
 package cli
 
+import "github.com/raedahgroup/dcrcli/config"
+
+type AppCommands struct {
+	config.Config
+	Balance    BalanceCommand    `command:"balance" description:"show your balance"`
+	Send       SendCommand       `command:"send" description:"send a transaction"`
+	SendCustom SendCustomCommand `command:"send-custom" description:"send a transaction, manually selecting inputs from unspent outputs"`
+	Receive    ReceiveCommand    `command:"receive" description:"show your address to receive funds"`
+	History    HistoryCommand    `command:"history" description:"show your transaction history"`
+}
+
 type BalanceCommand struct{}
 
 func (b BalanceCommand) Execute(args []string) error {
