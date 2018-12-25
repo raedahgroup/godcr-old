30426ae3d487a82cbaf93c96cca47ae7c9380d52
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2018-12-25 21:20:05 +0100

    implement AccountNumber function for mobilewallet

--- cli/commands/commands.go
+++ cli/commands/commands.go
@@ -3,8 +3,8 @@ package commands
 // CliCommands defines the commands and options available on the cli
 type CliCommands struct {
 	Balance    BalanceCommand    `command:"balance" description:"show your balance"`
+	Receive    ReceiveCommand    `command:"receive" description:"show your address to receive funds"`
 	Send       SendCommand       `command:"send" description:"send a transaction"`
 	SendCustom SendCustomCommand `command:"send-custom" description:"send a transaction, manually selecting inputs from unspent outputs"`
-	Receive    ReceiveCommand    `command:"receive" description:"show your address to receive funds"`
 	History    HistoryCommand    `command:"history" description:"show your transaction history"`
 }
