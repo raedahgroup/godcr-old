da5d1e0cc2109637643ba44db0c01cf83bf05a17
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2018-12-25 03:30:19 +0100

    add -d/--detailed flag to balance command

--- cli/commands/balance.go
+++ cli/commands/balance.go
@@ -4,18 +4,57 @@ import (
 	"fmt"
 	"github.com/decred/dcrd/dcrutil"
 	"github.com/raedahgroup/dcrcli/cli"
+	"github.com/raedahgroup/dcrcli/walletrpcclient"
 )
 
 // BalanceCommand displays the user's account balance.
-type BalanceCommand struct{}
+type BalanceCommand struct {
+	Detailed bool `short:"d" long:"detailed" description:"Display detailed account balance report"`
+}
 
 // Execute runs the `balance` command, displaying the user's account balance.
-func (b BalanceCommand) Execute(args []string) error {
+func (balanceCommand BalanceCommand) Execute(args []string) error {
 	accountBalances, err := cli.WalletClient.Balance()
 	if err != nil {
 		return err
 	}
 
+	if balanceCommand.Detailed {
+		showDetailedBalance(accountBalances)
+	} else {
+		showBalanceSummary(accountBalances)
+	}
+
+	return nil
+}
+
+func showDetailedBalance(accountBalances []*walletrpcclient.AccountBalanceResult) {
+	res := &cli.Response{
+		Columns: []string{
+			"Account",
+			"Total",
+			"Spendable",
+			"Locked By Tickets",
+			"Voting Authority",
+			"Unconfirmed",
+		},
+		Result: make([][]interface{}, len(accountBalances)),
+	}
+	for i, account := range accountBalances {
+		res.Result[i] = []interface{}{
+			account.AccountName,
+			account.Total,
+			account.Spendable,
+			account.LockedByTickets,
+			account.VotingAuthority,
+			account.Unconfirmed,
+		}
+	}
+
+	cli.PrintResult(cli.StdoutWriter, res)
+}
+
+func showBalanceSummary(accountBalances []*walletrpcclient.AccountBalanceResult) {
 	summarizeBalance := func(total, spendable dcrutil.Amount) string {
 		if total == spendable {
 			return total.String()
@@ -35,6 +74,4 @@ func (b BalanceCommand) Execute(args []string) error {
 		}
 		cli.PrintStringResult(commandOutput...)
 	}
-
-	return nil
 }
