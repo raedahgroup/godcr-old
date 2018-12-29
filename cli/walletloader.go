7c13433790defb706aa1a5922ae678eeb358d689
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2018-12-29 14:17:02 +0100

    propagate all cli errors to main for proper shutdown and exiting
    Former-commit-id: 6761cb0b6094560c589fa3313fda5dccc2681e5f

--- cli/walletloader.go
+++ cli/walletloader.go
@@ -1,7 +1,9 @@
 package cli
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"strings"
 	"sync"
 
@@ -10,36 +12,40 @@ import (
 )
 
 // createWallet creates a new wallet if one doesn't already exist using the WalletMiddleware provided
-func createWallet(walletMiddleware app.WalletMiddleware) {
+func createWallet(walletMiddleware app.WalletMiddleware) (err error) {
 	// first check if wallet already exists
 	walletExists, err := walletMiddleware.WalletExists()
 	if err != nil {
-		errMsg := fmt.Sprintf("Error checking %s wallet", walletMiddleware.NetType())
-		printErrorAndExit(errMsg, err)
+		fmt.Fprintf(os.Stderr, "Error checking %s wallet: %s\n", walletMiddleware.NetType(), err.Error())
+		return
 	}
 	if walletExists {
 		netType := strings.Title(walletMiddleware.NetType())
-		errMsg := fmt.Sprintf("%s wallet already exists", netType)
-		printErrorAndExit(errMsg, nil)
+		fmt.Fprintf(os.Stderr, "%s wallet already exists", netType)
+		return fmt.Errorf("wallet already exists")
 	}
 
 	// ask user to enter passphrase twice
 	passphrase, err := terminalprompt.RequestInputSecure("Enter private passphrase for new wallet", terminalprompt.EmptyValidator)
 	if err != nil {
-		printErrorAndExit("Error reading input", err)
+		fmt.Fprintf(os.Stderr, "Error reading input: %s", err.Error())
+		return
 	}
 	confirmPassphrase, err := terminalprompt.RequestInputSecure("Confirm passphrase", terminalprompt.EmptyValidator)
 	if err != nil {
-		printErrorAndExit("Error reading input", err)
+		fmt.Fprintf(os.Stderr, "Error reading input: %s\n", err.Error())
+		return
 	}
 	if passphrase != confirmPassphrase {
-		printErrorAndExit("Passphrases do not match", nil)
+		fmt.Fprintln(os.Stderr, "Passphrases do not match")
+		return fmt.Errorf("passphrases do not match")
 	}
 
 	// get seed and display to user
 	seed, err := walletMiddleware.GenerateNewWalletSeed()
 	if err != nil {
-		printErrorAndExit("Error generating seed for new wallet", err)
+		fmt.Fprintf(os.Stderr, "Error generating seed for new wallet: %s\n", err)
+		return
 	}
 	displayWalletSeed(seed)
 
@@ -56,65 +62,88 @@ func createWallet(walletMiddleware app.WalletMiddleware) {
 	}
 	_, err = terminalprompt.RequestInput(backupPrompt, backupValidator)
 	if err != nil {
-		printErrorAndExit("Error reading input", err)
+		fmt.Fprintf(os.Stderr, "Error reading input: %s", err.Error())
+		return
 	}
 
 	// user entered "OK" in last prompt, finalize wallet creation
 	err = walletMiddleware.CreateWallet(passphrase, seed)
 	if err != nil {
-		printErrorAndExit("Error creating wallet", err)
+		fmt.Fprintf(os.Stderr, "Error creating wallet: %s", err.Error())
+		return
 	}
 
 	fmt.Println("Your wallet has been created successfully")
 
 	// perform first blockchain sync after creating wallet
-	syncBlockChain(walletMiddleware)
+	return syncBlockChain(walletMiddleware)
 }
 
 // openWallet is called whenever an action to be executed requires wallet to be loaded
 // exits the program if wallet doesn't exist or some other error occurs
-func openWallet(walletMiddleware app.WalletMiddleware) {
-	walletExists, err := walletMiddleware.WalletExists()
-	if err != nil {
-		errMsg := fmt.Sprintf("Error checking %s wallet", walletMiddleware.NetType())
-		printErrorAndExit(errMsg, err)
-	}
+// especially in cases of multiple mobilewallet instances, this method may stall until previous dcrcli instances are closed
+func openWallet(ctx context.Context, walletMiddleware app.WalletMiddleware) error {
+	var err error
+	var errMsg string
+	loadWalletDone := make(chan bool)
 
-	if !walletExists {
-		netType := strings.Title(walletMiddleware.NetType())
-		errMsg := fmt.Sprintf("%s wallet does not exist. Create it using '%s --createwallet'", netType, app.Name())
-		printErrorAndExit(errMsg, nil)
-	}
+	go func() {
+		defer func() {
+			loadWalletDone <- true
+		}()
 
-	err = walletMiddleware.OpenWallet()
-	if err != nil {
-		errMsg := fmt.Sprintf("Failed to open %s wallet", walletMiddleware.NetType())
-		printErrorAndExit(errMsg, err)
+		var walletExists bool
+		walletExists, err = walletMiddleware.WalletExists()
+		if err != nil {
+			errMsg = fmt.Sprintf("Error checking %s wallet", walletMiddleware.NetType())
+			return
+		}
+
+		if !walletExists {
+			netType := strings.Title(walletMiddleware.NetType())
+			errMsg = fmt.Sprintf("%s wallet does not exist. Create it using '%s --createwallet'", netType, app.Name())
+			return
+		}
+
+		err = walletMiddleware.OpenWallet()
+		if err != nil {
+			errMsg = fmt.Sprintf("Failed to open %s wallet", walletMiddleware.NetType())
+			return
+		}
+	}()
+
+	select {
+	case <- loadWalletDone:
+		if errMsg != "" {
+			fmt.Fprintln(os.Stderr, errMsg)
+		}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+		return err
+
+	case <- ctx.Done():
+		return ctx.Err()
 	}
 }
 
 // syncBlockChain uses the WalletMiddleware provided to download block updates
-// causes app to exit if an error is encountered
-func syncBlockChain(walletMiddleware app.WalletMiddleware) {
-	var err error
-	defer func() {
-		if err != nil {
-			printErrorAndExit("Error syncing blockchain", err)
-		} else {
-			fmt.Println("Blockchain synced successfully")
-		}
-	}()
-
+func syncBlockChain(walletMiddleware app.WalletMiddleware) (err error) {
 	// use wait group to wait for go routine process to complete before exiting this function
 	var wg sync.WaitGroup
 	wg.Add(1)
 
 	err = walletMiddleware.SyncBlockChain(&app.BlockChainSyncListener{
 		SyncStarted: func() {
-			fmt.Println("Starting blockchain sync")
+			fmt.Println("Blockchain sync started")
 		},
 		SyncEnded: func(e error) {
 			err = e
+			if err == nil {
+				fmt.Println("Blockchain synced successfully")
+			} else {
+				fmt.Fprintf(os.Stderr, "Blockchain sync completed with error: %s", err.Error())
+			}
 			wg.Done()
 		},
 		OnHeadersFetched:    func(percentageProgress int64) {}, // in cli mode, sync updates are logged to terminal, no need to act on this update alert
@@ -129,4 +158,5 @@ func syncBlockChain(walletMiddleware app.WalletMiddleware) {
 		// sync in progress, wait for BlockChainSyncListener.OnComplete
 		wg.Wait()
 	}
+	return
 }
