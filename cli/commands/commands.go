43e2f19188f0aef1a6e4b0a13011cc5983203519
Ademu Enyo Anthony <ademuanthony@gmail.com>
2019-01-01 13:00:59 +0100

    cleanup

--- cli/commands/commands.go
+++ cli/commands/commands.go
@@ -3,13 +3,13 @@ package commands
 // Commands defines the commands and options available on the cli
 type Commands struct {
 	CreateWallet    CreateWalletCommand    `command:"create-wallet" description:"Creates a new decred testnet or mainnet wallet" long-description:"Creates a new decred testnet or mainnet wallet. A wallet seed will be generated for the new wallet which must be stored securely. You'll also be asked to set a password for the wallet"`
-	Balance         BalanceCommand         `command:"balance" description:"Show total balance for each account in wallet" long-description:"Shows total balance for each account in wallet. Also shows spendable balance if different from total balance. Use -d/--detailed to view a more detailed balance breakdown"`
+	Balance         BalanceCommand         `command:"balance" description:"Show total balance for each account in wallet" long-description:"Also shows spendable balance if different from total balance"`
 	Send            SendCommand            `command:"send" description:"Send a transaction"`
 	SendCustom      SendCustomCommand      `command:"send-custom" description:"Send a transaction, manually selecting inputs from unspent outputs"`
 	Receive         ReceiveCommand         `command:"receive" description:"Show your address to receive funds"`
 	History         HistoryCommand         `command:"history" description:"Show your transaction history"`
 	ShowTransaction ShowTransactionCommand `command:"show-transaction" description:"Show details of a transaction"`
-	Help            HelpCommand            `command:"help" description:"Show application help, run 'help' for general help 'help <command-name>' for command-specific help."`
+	Help            HelpCommand            `command:"help" description:"Show general application help. Run help <command-name> to get help message for a specific command"`
 }
 
 // commanderStub implements `flags.Commander`, using a noop Execute method to satisfy `flags.Commander` interface
