c97c5d048106956f3736a4439c3052a678cf0b53
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2019-01-11 18:04:13 +0100

    make detect wallets command not depend on anything

--- cli/commands/detect-wallets.go
+++ cli/commands/detect-wallets.go
@@ -1,19 +1,15 @@
 package commands
 
 import (
-	"context"
 	"fmt"
 	"github.com/raedahgroup/godcr/app/config"
-	"github.com/raedahgroup/godcr/app/walletcore"
 	"os"
 	"path/filepath"
 )
 
-type DetectWalletsCommand struct {
-	commanderStub
-}
+type DetectWalletsCommand struct {}
 
-func (detectCmd DetectWalletsCommand) Run(ctx context.Context, wallet walletcore.Wallet) error {
+func (detectCmd DetectWalletsCommand) Execute(args []string) error {
 	err := filepath.Walk("/usr/lib", func(path string, file os.FileInfo, err error) error {
 		if err == nil && !file.IsDir() && file.Name() == config.WalletDbFileName {
 			fmt.Println(path)
