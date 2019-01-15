5bec7c310a8aca53c4fcdb050df9de4943d18f55
Raphael <raphaelint@gmail.com>
2019-01-15 12:55:45 +0100

    Skeletal framework tview

---
+++ terminal/terminal.go
@@ -0,0 +1,44 @@
+package terminal
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/rivo/tview"
+	"github.com/raedahgroup/godcr/cli/walletloader"
+	"github.com/raedahgroup/godcr/app"
+)
+
+func StartTerminalApp(ctx context.Context, walletMiddleware app.WalletMiddleware) error {
+	fmt.Sprintf("%s Terminal", strings.ToUpper(app.Name)) //Print App name
+
+	tviewApp := tview.NewApplication()
+
+	list := tview.NewList().
+		AddItem("Balance", "", 'b', nil).
+		AddItem("Receive", "", 'r', nil).
+		AddItem("Send", "", 's', nil).
+		AddItem("History", "", 'h', nil).
+		AddItem("Exit", "", 'q', func() {
+		tviewApp.Stop()
+		})
+	list.SetBorder(true).SetTitle(strings.ToUpper(app.Name))
+
+	err := syncBlockChain(ctx, walletMiddleware)
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	// `Run` blocks until app.Stop() is called before returning
+	return tviewApp.SetRoot(list, true).SetFocus(list).Run()
+}
+
+func syncBlockChain(ctx context.Context, walletMiddleware app.WalletMiddleware) error {
+	_, err := walletloader.OpenWallet(ctx, walletMiddleware)
+	if err != nil {
+		return err
+	}
+
+	return walletloader.SyncBlockChain(ctx, walletMiddleware)
+}
