ab0bd3b49b957ed29a98d11b1a13f2ac279bbd6b
Raphael <raphaelint@gmail.com>
2019-01-24 17:58:09 +0000

    Creating grid layout

--- terminal/terminal.go
+++ terminal/terminal.go
@@ -10,17 +10,13 @@ import (
 	"github.com/rivo/tview"
 )
 
+var tviewApp *tview.Application
+
 func StartTerminalApp(ctx context.Context, walletMiddleware app.WalletMiddleware) error {
-	tviewApp := tview.NewApplication()
-	list := tview.NewList().
-		AddItem("Balance", "", 'b', nil).
-		AddItem("Receive", "", 'r', nil).
-		AddItem("Send", "", 's', nil).
-		AddItem("History", "", 'h', nil).
-		AddItem("Exit", "", 'q', func() {
-		tviewApp.Stop()
-		})
-	list.SetBorder(true).SetTitle(fmt.Sprintf("%s Terminal", strings.ToUpper(app.Name)))
+	tviewApp = tview.NewApplication()
+	
+	// Grid Layout Structure for screens
+	tviewGrid := GridLayout()
 
 	err := syncBlockChain(ctx, walletMiddleware)
 	if err != nil{
@@ -28,7 +24,41 @@ func StartTerminalApp(ctx context.Context, walletMiddleware app.WalletMiddleware
 	}
 
 	// `Run` blocks until app.Stop() is called before returning
-	return tviewApp.SetRoot(list, true).SetFocus(list).Run()
+	return tviewApp.SetRoot(tviewGrid, true).SetFocus(tviewGrid).Run()
+}
+
+func GridLayout() tview.Primitive {
+
+	PagesColumn := func(text string) tview.Primitive {
+		return tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(text)
+	}
+
+	//List of menu that will display at the left hand
+	MenuColumn := func() tview.Primitive {
+		return tview.NewList().
+			AddItem("Balance", "", 'b', nil).
+			AddItem("Receive", "", 'r', nil).
+			AddItem("Send", "", 's', nil).
+			AddItem("History", "", 'h', nil).
+			AddItem("Exit", "", 'q', func() {
+			tviewApp.Stop()
+		})
+	}
+
+	newmenu := MenuColumn()
+	newpages := PagesColumn("Pages")
+	header := tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("%s Terminal", strings.ToUpper(app.Name)))
+
+	gridApp := tview.NewGrid().SetBorders(true).SetRows(3, 0).SetColumns(30, 0)
+
+	// First Row With single Column
+	gridApp.AddItem(header, 0, 0, 1, 3, 0, 0, false)
+
+	// Second Row with two column
+	gridApp.AddItem(newmenu, 1, 0, 1, 1, 0, 0, true).
+			AddItem(newpages, 1, 1, 1, 2, 0, 0, false)
+
+	return gridApp
 }
 
 func syncBlockChain(ctx context.Context, walletMiddleware app.WalletMiddleware) error {
