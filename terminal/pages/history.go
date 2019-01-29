0997d19d93cb2890a8936819c253805bccdb9df0
Raphael <raphaelint@gmail.com>
2019-01-29 15:53:32 +0100

    terminal_layout_adding_pages_History

--- terminal/pages/history.go
+++ terminal/pages/history.go
@@ -1,1 +1,22 @@
 package pages
+
+import (
+	"github.com/rivo/tview"
+)
+
+func HistoryPage() tview.Primitive {
+	title := pageTitle("History")
+
+	body := tview.NewTable().SetBorders(true)
+	body.SetCell(0, 0, tview.NewTableCell("Date").SetAlign(tview.AlignCenter))
+	body.SetCell(0, 1, tview.NewTableCell("Amount").SetAlign(tview.AlignCenter))
+	body.SetCell(0, 2, tview.NewTableCell("Fee").SetAlign(tview.AlignCenter))
+	body.SetCell(0, 3, tview.NewTableCell("Direction").SetAlign(tview.AlignCenter))
+	body.SetCell(0, 4, tview.NewTableCell("Type").SetAlign(tview.AlignCenter))
+
+	gridReceive := tview.NewGrid().SetRows(2, 0).SetColumns(0)
+	gridReceive.AddItem(title, 0, 0, 1, 1, 0, 0, true).SetBorders(false)
+	gridReceive.AddItem(body, 1, 0, 1, 1, 0, 0, true).SetBorders(false)
+
+	return gridReceive
+}
--- terminal/pages/receive.go
+++ terminal/pages/receive.go
@@ -8,7 +8,9 @@ import (
 
 func ReceivePage() tview.Primitive {
 	title := pageTitle("Receive")
+
 	body := tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("ID : %s","TsU1YvSmtqw7wUtsRvSWjVs9BRxfT7urLzN"))
+	
 	gridReceive := tview.NewGrid().SetRows(2, 0).SetColumns(0)
 	gridReceive.AddItem(title, 0, 0, 1, 1, 0, 0, true).SetBorders(false)
 	gridReceive.AddItem(body, 1, 0, 1, 1, 0, 0, true).SetBorders(false)
