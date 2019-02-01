12301a28319d3932743b720c99e19a7e68178a85
Ademu Enyo Anthony <ademuanthony@gmail.com>
2019-02-01 15:42:55 +0100

    requested changes

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,11 @@
 import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
+import './css/style.scss'
+
+import jquery from 'jquery'
+import 'bootstrap'
+window.jQuery = jquery
+window.$ = jquery
 
 const application = Application.start()
 const context = require.context('./controllers', true, /\.js$/)
--- web/webpack/webpack.common.js
+++ web/webpack/webpack.common.js
@@ -1,14 +1,15 @@
 const path = require('path')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const StyleLintPlugin = require('stylelint-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: {
     app: './src/index.js'
   },
   externals: {
-    jquery: 'jQuery',
-    turbolinks: 'Turbolinks'
+    jquery: 'jquery',
+    bootstrap: 'bootstrap'
   },
   optimization: {
     splitChunks: {
@@ -16,11 +17,50 @@ module.exports = {
     }
   },
   module: {
-    
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new StyleLintPlugin()
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      // filename: '[name].css',
+      // chunkFilename: '[id].css'
+      filename: 'css/style.css'
+    }),
+    new StyleLintPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
   ],
   output: {
     filename: 'js/[name].bundle.js',
