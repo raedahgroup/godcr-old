6e512615f3738fbc6c1098544d653897b0d9fff0
Raphael <raphaelint@gmail.com>
2019-02-04 12:43:31 +0100

    Making correct on send page

--- terminal/pages/send.go
+++ terminal/pages/send.go
@@ -10,7 +10,7 @@ import (
 func SendPage(setFocus func(p tview.Primitive) *tview.Application, clearFocus func()) tview.Primitive {
 	//Form for Sending
 	body := tview.NewForm().
-		AddDropDown("Account", []string{"Dafault", "..."}, 0, nil).
+		AddDropDown("Account", []string{"Default", "..."}, 0, nil).
 		AddInputField("Amount", "", 20, nil, nil).
 		AddInputField("Destination Address", "", 20, nil, nil).
 		AddButton("Send", func() {
