76a08f338b0d0a25f7b9b746d807a737658d0b91
Raphael <raphaelint@gmail.com>
2019-02-04 12:31:43 +0100

    Terminal ui branding push

--- terminal/pages/history.go
+++ terminal/pages/history.go
@@ -1,20 +1,18 @@
 package pages
 
 import (
-	"github.com/rivo/tview"
 	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
 )
 
 func HistoryPage() tview.Primitive {
+	color := tcell.NewRGBColor(255, 255, 255)
 	body := tview.NewTable().SetBorders(true)
-	body.SetCell(0, 0, tview.NewTableCell("Date").SetAlign(tview.AlignCenter))
-	body.SetCell(0, 1, tview.NewTableCell("Amount").SetAlign(tview.AlignCenter))
-	body.SetCell(0, 2, tview.NewTableCell("Fee").SetAlign(tview.AlignCenter))
-	body.SetCell(0, 3, tview.NewTableCell("Direction").SetAlign(tview.AlignCenter))
-	body.SetCell(0, 4, tview.NewTableCell("Type").SetAlign(tview.AlignCenter))
-	body.SetBackgroundColor(tcell.NewRGBColor(255, 255, 255))
-	body.SetBorderColor(tcell.NewRGBColor(0, 0, 51))
-	body.SetBorderAttributes(tcell.AttrBold)
+	body.SetCell(0, 0, tview.NewTableCell("Date").SetAlign(tview.AlignCenter).SetTextColor(color))
+	body.SetCell(0, 1, tview.NewTableCell("Amount").SetAlign(tview.AlignCenter).SetTextColor(color))
+	body.SetCell(0, 2, tview.NewTableCell("Fee").SetAlign(tview.AlignCenter).SetTextColor(color))
+	body.SetCell(0, 3, tview.NewTableCell("Direction").SetAlign(tview.AlignCenter).SetTextColor(color))
+	body.SetCell(0, 4, tview.NewTableCell("Type").SetAlign(tview.AlignCenter).SetTextColor(color))
 
 	return body
 }
