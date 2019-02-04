5fae194fa6afdd3de80f2996e1924ab5945b0651
Raphael <raphaelint@gmail.com>
2019-02-04 12:31:43 +0100

    Terminal ui branding push

--- terminal/pages/balance.go
+++ terminal/pages/balance.go
@@ -4,13 +4,10 @@ import (
 	"fmt"
 
 	"github.com/rivo/tview"
-	"github.com/gdamore/tcell"
 )
 
 func BalancePage() tview.Primitive {
-	body := tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("Balance : %s", "0 GODCR"))
-	body.SetTextColor(tcell.NewRGBColor(0, 0, 0))
-	body.SetBackgroundColor(tcell.NewRGBColor(255, 255, 255))
+	body := tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("\n\n\nBalance : %s", "0 GODCR"))
 
 	return body
 }
--- terminal/pages/receive.go
+++ terminal/pages/receive.go
@@ -4,14 +4,10 @@ import (
 	"fmt"
 
 	"github.com/rivo/tview"
-	"github.com/gdamore/tcell"
 )
 
 func ReceivePage() tview.Primitive {
-	body := tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("ID : %s", "TsU1YvSmtqw7wUtsRvSWjVs9BRxfT7urLzN"))
-	body.SetTextColor(tcell.NewRGBColor(0, 0, 0))
-	body.SetBackgroundColor(tcell.NewRGBColor(255, 255, 255))
+	body := tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("\n\n\nID : %s", "TsU1YvSmtqw7wUtsRvSWjVs9BRxfT7urLzN"))
 
 	return body
 }
-
