23e58bcf664063bedeeeff7f5f93aa584b1ed013
edono morrison <edonomorrison@gmail.com>
2019-02-21 20:41:02 +0100

    added show detailed functionality

--- terminal/pages/balance.go
+++ terminal/pages/balance.go
@@ -1,41 +1,9 @@
 package pages
 
 import (
-	// "fmt"
-
-	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 	"github.com/raedahgroup/godcr/app/walletcore"
 )
-
-// func BalancePage(wallet walletcore.Wallet, setFocus func(p tview.Primitive) *tview.Application, clearFocus func()) tview.Primitive {
-// body := tview.NewPages()
-// textView := tview.NewTextView()
-// flex := tview.NewFlex().SetDirection(tview.FlexRow)
-// // table := tview.NewTable().SetBorders(true)
-// checkbox := tview.NewCheckbox()	
-// accounts, err := wallet.AccountsOverview(walletcore.DefaultRequiredConfirmations)
-// if err != nil {
-// 	return textView.SetText(err.Error())
-// }
-
-// var output string
-// if len(accounts) == 1 {
-// 	output = walletcore.SimpleBalance(accounts[0].Balance, false)
-// }
-// fmt.Println(output)
-
-// body.AddPage(fmt.Sprintf("Balance Page"),
-// 	flex.AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-// 		AddItem(textView.SetTextAlign(tview.AlignCenter).SetText(output), 1, 1, false).
-// 		AddItem(checkbox.SetLabel("View Detailed Balance  ").SetChecked(false).SetChangedFunc(func(checked bool) {}), 0, 1, false), 0, 2, false),
-// 	false,
-// 	true)
-
-// setFocus(body)
-// return body
-// }
-
 
 func BalancePage(wallet walletcore.Wallet, setFocus func(p tview.Primitive) *tview.Application, clearFocus func()) tview.Primitive {
 	textView := tview.NewTextView()
@@ -48,38 +16,42 @@ func BalancePage(wallet walletcore.Wallet, setFocus func(p tview.Primitive) *tvi
 		return textView.SetText(err.Error())
 	}
 
-	// body.AddItem(tview.NewFlex().SetDirection(tview.FlexRow)
-	body.AddItem(checkbox.SetLabel("View Detailed Balance  ").SetChecked(false).SetChangedFunc(func(checked bool) {
-		if checked != true {
-			checkbox.Draw(tcell.Clear())
+	body.AddItem(checkbox.SetLabel("View Detailed Balance  ").SetChecked(false).SetChangedFunc(func(checked bool){
+		if (checked != false) {
+			body.RemoveItem(table)
+			body.RemoveItem(textView)
+
+			for _, account := range accounts {
+			body.AddItem(
+				table.SetCell(0, 0, tview.NewTableCell("Account Name").SetAlign(tview.AlignCenter)).
+				SetCell(0, 1, tview.NewTableCell("Balance").SetAlign(tview.AlignCenter)).
+				SetCell(0, 2, tview.NewTableCell("Spendable").SetAlign(tview.AlignCenter)).
+				SetCell(0, 3, tview.NewTableCell("Locked").SetAlign(tview.AlignCenter)).
+				SetCell(0, 4, tview.NewTableCell("Voting Authority").SetAlign(tview.AlignCenter)).
+				SetCell(0, 5, tview.NewTableCell("Unconfirmed").SetAlign(tview.AlignCenter)).
+				SetCell(1, 0, tview.NewTableCell(account.Name).SetAlign(tview.AlignCenter)).
+				SetCell(1, 1, tview.NewTableCell(walletcore.SimpleBalance(account.Balance, true)).SetAlign(tview.AlignCenter)).
+				SetCell(1, 2, tview.NewTableCell(account.Balance.Spendable.String()).SetAlign(tview.AlignCenter)).
+				SetCell(1, 3, tview.NewTableCell(account.Balance.LockedByTickets.String()).SetAlign(tview.AlignCenter)).
+				SetCell(1, 4, tview.NewTableCell(account.Balance.VotingAuthority.String()).SetAlign(tview.AlignCenter)).
+				SetCell(1, 5, tview.NewTableCell(account.Balance.Unconfirmed.String()).SetAlign(tview.AlignCenter)), 0, 2, true)}
+		}else{
+			 body.RemoveItem(table)
 		}
-		for _, account := range accounts {
-		body.AddItem(
-			table.SetCell(0, 0, tview.NewTableCell("Account Name").SetAlign(tview.AlignCenter)).
-			SetCell(0, 1, tview.NewTableCell("Balance").SetAlign(tview.AlignCenter)).
-			SetCell(0, 2, tview.NewTableCell("Spendable").SetAlign(tview.AlignCenter)).
-			SetCell(0, 3, tview.NewTableCell("Locked").SetAlign(tview.AlignCenter)).
-			SetCell(0, 4, tview.NewTableCell("Voting Authority").SetAlign(tview.AlignCenter)).
-			SetCell(0, 5, tview.NewTableCell("Unconfirmed").SetAlign(tview.AlignCenter)).
-			SetCell(1, 0, tview.NewTableCell(account.Name).SetAlign(tview.AlignCenter)).
-			SetCell(1, 1, tview.NewTableCell(walletcore.SimpleBalance(account.Balance, true)).SetAlign(tview.AlignCenter)).
-			SetCell(1, 2, tview.NewTableCell(account.Balance.Spendable.String()).SetAlign(tview.AlignCenter)).
-			SetCell(1, 3, tview.NewTableCell(account.Balance.LockedByTickets.String()).SetAlign(tview.AlignCenter)).
-			SetCell(1, 4, tview.NewTableCell(account.Balance.VotingAuthority.String()).SetAlign(tview.AlignCenter)).
-			SetCell(1, 5, tview.NewTableCell(account.Balance.Unconfirmed.String()).SetAlign(tview.AlignCenter)), 0, 2, false)}
 	}), 3, 1, true)
+
 	if len(accounts) == 1 {
 		output := walletcore.SimpleBalance(accounts[0].Balance, false)
-		body.AddItem(textView.SetTextAlign(tview.AlignCenter).SetText(output), 1, 1, false)
+		body.AddItem(textView.SetTextAlign(tview.AlignCenter).SetText(output), 5, 1, false)
 	}else{
-		for _, account := range accounts {
-		body.AddItem(
-			table.SetCell(0, 0, tview.NewTableCell("Account Name").SetAlign(tview.AlignCenter)).
-			SetCell(0, 1, tview.NewTableCell("Balance").SetAlign(tview.AlignCenter)).
-			SetCell(1, 0, tview.NewTableCell(account.Name).SetAlign(tview.AlignCenter)).
-			SetCell(1, 1, tview.NewTableCell(walletcore.SimpleBalance(account.Balance, true)).SetAlign(tview.AlignCenter)), 0, 2, false)}
+	for _, account := range accounts {
+	body.AddItem(
+		table.SetCell(0, 0, tview.NewTableCell("Account Name").SetAlign(tview.AlignCenter)).
+		SetCell(0, 1, tview.NewTableCell("Balance").SetAlign(tview.AlignCenter)).
+		SetCell(1, 0, tview.NewTableCell(account.Name).SetAlign(tview.AlignCenter)).
+		SetCell(1, 1, tview.NewTableCell(walletcore.SimpleBalance(account.Balance, true)).SetAlign(tview.AlignCenter)), 0, 2, true)}
 	}
-
+	
 	setFocus(body)
 	return body
 }
