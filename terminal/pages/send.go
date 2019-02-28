5c370a1bee6ee64f51f975119f7351de91842893
edono morrison <edonomorrison@gmail.com>
2019-02-28 17:10:16 +0100

    added flexbox and restructured send page

--- terminal/pages/send.go
+++ terminal/pages/send.go
@@ -2,24 +2,23 @@ package pages
 
 import (
 	"fmt"
-	// "errors"
-    // "strconv"
 
 	"github.com/rivo/tview"
 	"github.com/raedahgroup/godcr/app/walletcore"
 )
 
 func SendPage(wallet walletcore.Wallet, setFocus func(p tview.Primitive) *tview.Application, clearFocus func()) tview.Primitive {
-	errMsg := tview.NewTextView().SetTextAlign(tview.AlignCenter)
-	body := tview.NewForm()
+	body := tview.NewFlex().SetDirection(tview.FlexRow)
+	// textView := tview.NewTextView().SetTextAlign(tview.AlignCenter)
+	form := tview.NewForm()
 
 	accounts, err := wallet.AccountsOverview(walletcore.DefaultRequiredConfirmations)
 	if err != nil {
-		return errMsg.SetText(err.Error())
+		return body.AddItem(tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("Error: %s", err.Error())), 0, 1, false)
 	}
 
 	//Form for Sending
-	var  amount, destination string 
+	var  amount, destination string
 	var accountNum uint32
 	var checked bool
 
@@ -27,37 +26,37 @@ func SendPage(wallet walletcore.Wallet, setFocus func(p tview.Primitive) *tview.
 	accountN := make([]uint32, len(accounts))
 	for index, account := range accounts {
 		accountNames[index] = account.Name
-		body.AddDropDown("Account", []string{accountNames[index]}, 0, func(option string, optionIndex int) {
+		body.AddItem(form.AddDropDown("Account", []string{accountNames[index]}, 0, func(option string, optionIndex int) {
 			accountNum = accountN[optionIndex]
-		})
+		}).
+		AddInputField("Amount", "", 20, nil, func (text string) {
+			// if text == "" {
+			// 	// errMsg.SetText("field cannot be  0")
+			// 	// return
+			// }
+			amount = text
+		}).
+		AddInputField("Destination Address", "", 20, nil, func (text string) {
+			destination = text
+		}).
+		AddCheckbox("Unconfirmed", false, func(checked bool) {
+			if checked != false {
+				checked = true
+			}
+		}).
+		AddPasswordField("Password", "", 20, '*', nil).
+		AddButton("Send", func() {
+			err := confBalance(accountNum, wallet)
+			if err != nil {
+				body.AddItem(tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("Error: %s", err.Error())), 0, 1, false)
+				return 
+			}
+			fmt.Println(destination, amount, checked)
+		}).
+		AddButton("Cancel", func() {
+			clearFocus()
+		}), 0, 1, true)
 	}
-	body.AddInputField("Amount", "", 20, nil, func (text string) {
-		if text == "" {
-			errMsg.SetText("field cannot be  0")
-			return
-		}
-		amount = text
-	}).
-	AddInputField("Destination Address", "", 20, nil, func (text string) {
-		destination = text
-	}).
-	AddCheckbox("Unconfirmed", false, func(checked bool) {
-		if checked != false {
-			checked = true
-		}
-	}).
-	AddButton("Send", func() {
-		err := confBalance(accountNum, wallet)
-		if err != nil {
-			errMsg.SetText(err.Error())
-			// fmt.Println(err.Error())
-			return 
-		}
-		fmt.Println(destination, amount, checked)
-	}).
-	AddButton("Cancel", func() {
-		clearFocus()
-	})
 
 	setFocus(body)
 	
