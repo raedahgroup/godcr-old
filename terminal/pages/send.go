68aaf8a5808b733ede33bcca0740f8dcf7a9839a
edono morrison <edonomorrison@gmail.com>
2019-03-02 20:10:58 +0100

    pulled wallet data, and impeoved form fields and data validation

--- terminal/pages/send.go
+++ terminal/pages/send.go
@@ -2,8 +2,10 @@ package pages
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/rivo/tview"
+	"github.com/raedahgroup/dcrlibwallet/txhelper"
 	"github.com/raedahgroup/godcr/app/walletcore"
 )
 
@@ -12,46 +14,68 @@ func SendPage(wallet walletcore.Wallet, setFocus func(p tview.Primitive) *tview.
 	// textView := tview.NewTextView().SetTextAlign(tview.AlignCenter)
 	form := tview.NewForm()
 
+	body.AddItem(tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText("Send Fund"), 4, 1, false)
+	
 	accounts, err := wallet.AccountsOverview(walletcore.DefaultRequiredConfirmations)
 	if err != nil {
 		return body.AddItem(tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("Error: %s", err.Error())), 0, 1, false)
 	}
 
 	//Form for Sending
-	var  amount, destination string
+	var  amount, destination []string
+	var Passphrase string
 	var accountNum uint32
-	var checked bool
+	var spendUnconfirmed bool
 
 	accountNames := make([]string, len(accounts))
 	accountN := make([]uint32, len(accounts))
 	for index, account := range accounts {
-		accountNames[index] = account.Name
+		accountNames[index] = fmt.Sprintf("%s - %s ", account.Name, account.Balance.Total.String())
 		body.AddItem(form.AddDropDown("Account", []string{accountNames[index]}, 0, func(option string, optionIndex int) {
 			accountNum = accountN[optionIndex]
 		}).
 		AddInputField("Amount", "", 20, nil, func (text string) {
-			// if text == "" {
-			// 	// errMsg.SetText("field cannot be  0")
-			// 	// return
-			// }
 			amount = text
 		}).
 		AddInputField("Destination Address", "", 20, nil, func (text string) {
 			destination = text
 		}).
 		AddCheckbox("Unconfirmed", false, func(checked bool) {
-			if checked != false {
-				checked = true
+			if checked {
+				spendUnconfirmed = true
 			}
 		}).
-		AddPasswordField("Password", "", 20, '*', nil).
+		AddCheckbox("Select custom inputs", false, func(checked bool) {
+			//todo add select custom inputs to send page
+		}).
+		AddPasswordField("Wallet Passphrase", "", 20, '*', func (text string) {
+			Passphrase = text
+		}).
 		AddButton("Send", func() {
-			err := confBalance(accountNum, wallet)
+			sendDestinations := make([]txhelper.TransactionDestination, len(destination))
+			for i := range destination {
+				Amount, err := strconv.ParseFloat(string(amount[i]), 32)
+				if err != nil {
+					body.AddItem(tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("Error: %s", err.Error())), 0, 1, false)
+					return
+				}
+				sendDestinations[i] = txhelper.TransactionDestination{
+					Address: destination[i],
+					Amount:  amount,
+				}
+			}
+
+			var requiredConfirmations int32 = walletcore.DefaultRequiredConfirmations
+			if spendUnconfirmed {
+				requiredConfirmations = 0
+			}
+
+			txHash, err = routes.walletMiddleware.SendFromAccount(accountNum, requiredConfirmations, sendDestinations, passphrase)
 			if err != nil {
 				body.AddItem(tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText(fmt.Sprintf("Error: %s", err.Error())), 0, 1, false)
-				return 
+				return
 			}
-			fmt.Println(destination, amount, checked)
+			fmt.Println(txHash)
 		}).
 		AddButton("Cancel", func() {
 			clearFocus()
