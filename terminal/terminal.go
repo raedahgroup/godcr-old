3f50c3b0804a9b4efe00f990d01330791e668775
edono morrison <edonomorrison@gmail.com>
2019-03-09 23:43:00 +0100

    modified syncBlockChain function my calling it into the terminal UI instead of writing to stdOut

--- terminal/terminal.go
+++ terminal/terminal.go
@@ -7,9 +7,11 @@ import (
 
 	"github.com/gdamore/tcell"
 	"github.com/raedahgroup/godcr/app"
+	"github.com/raedahgroup/godcr/app/walletcore"
 	"github.com/raedahgroup/godcr/terminal/helpers"
 	"github.com/raedahgroup/godcr/terminal/pages"
 	"github.com/raedahgroup/godcr/terminal/primitives"
+
 	"github.com/rivo/tview"
 )
 
@@ -17,13 +19,94 @@ func StartTerminalApp(ctx context.Context, walletMiddleware app.WalletMiddleware
 	tviewApp := tview.NewApplication()
 
 	page := pageLoader(ctx, tviewApp, walletMiddleware)
+	// `Run` blocks until app.Stop() is called before returning
 	return tviewApp.SetRoot(page, true).Run()
+}
+
+var Report string
+var Status walletcore.SyncStatus
+
+func terminalLayout(ctx context.Context, tviewApp *tview.Application, walletMiddleware app.WalletMiddleware) tview.Primitive {
+
+	syncBlockchain(walletMiddleware)
+	if Status == walletcore.SyncStatusError {
+		msgOutput := fmt.Sprintf("Cannot display page. %s", Report)
+		return tview.NewTextView().SetText(msgOutput).SetTextAlign(tview.AlignCenter)
+	}
+	if Status == walletcore.SyncStatusInProgress {
+		msgOutput := fmt.Sprintf("%s. Refresh after a while to access this page", Report)
+		return tview.NewTextView().SetText(msgOutput).SetTextAlign(tview.AlignCenter)
+	}
+	if Status == walletcore.SyncStatusSuccess {
+
+	header := helpers.CenterAlignedTextView(fmt.Sprintf("\n%s Terminal", strings.ToUpper(app.Name)))
+	header.SetBackgroundColor(helpers.DecredColor)
+		//Creating the View for the Layout
+	gridLayout := tview.NewGrid().SetRows(3, 0).SetColumns(30, 0)
+	//Controls the display for the right side column
+	var activePage tview.Primitive
+	changePageColumn := func(page tview.Primitive) {
+		gridLayout.RemoveItem(activePage)
+		activePage = page
+		gridLayout.AddItem(activePage, 1, 1, 1, 1, 0, 0, true)
+	}
+	var menuColumn *tview.List
+	setFocus := tviewApp.SetFocus
+
+	menuColumn := tview.NewList()
+	clearFocus := func() {
+		gridLayout.RemoveItem(activePage)
+		tviewApp.SetFocus(menuColumn)
+	}
+
+	//Menu List of the Layout
+	menuColumn.AddItem("Balance", "", 'b', func() {
+		changePageColumn(pages.BalancePage(walletMiddleware, setFocus, clearFocus))
+	})
+
+	menuColumn.AddItem("Receive", "", 'r', func() {
+		changePageColumn(pages.ReceivePage(walletMiddleware, setFocus, clearFocus))
+	})
+
+	menuColumn.AddItem("Send", "", 's', func() {
+		changePageColumn(pages.SendPage(setFocus, clearFocus))
+	})
+
+	menuColumn.AddItem("History", "", 'h', func() {
+		changePageColumn(pages.HistoryPage(walletMiddleware, setFocus, clearFocus))
+	})
+
+	menuColumn.AddItem("Stake Info", "", 'k', func() {
+		changePageColumn(pages.StakeinfoPage(walletMiddleware, setFocus, clearFocus))
+	})
+
+	menuColumn.AddItem("Purchase Tickets", "", 't', func() {
+		changePageColumn(pages.PurchaseTicketsPage(walletMiddleware, setFocus, clearFocus))
+	})
+
+	menuColumn.AddItem("Exit", "", 'q', func() {
+		tviewApp.Stop()
+	})
+
+	menuColumn.SetCurrentItem(0)
+	menuColumn.SetShortcutColor(helpers.DecredLightColor)
+	menuColumn.SetBorder(true)
+	menuColumn.SetBorderColor(helpers.DecredLightColor)
+	// Layout for screens Header
+	gridLayout.AddItem(header, 0, 0, 1, 2, 0, 0, false)
+	// Layout for screens with two column
+	gridLayout.AddItem(menuColumn, 1, 0, 1, 1, 0, 0, true)
+	changePageColumn(pages.BalancePage(walletMiddleware, setFocus, clearFocus))
+	gridLayout.SetBordersColor(helpers.DecredLightColor)
+
+	return gridLayout
+}
 }
 
 func pageLoader(ctx context.Context, tviewApp *tview.Application, walletMiddleware app.WalletMiddleware) tview.Primitive {
 	walletExists, err := openWalletIfExist(ctx, walletMiddleware)
 	if err != nil {
-		tview.NewTextView().SetText(err.Error()).SetTextAlign(tview.AlignCenter)
+		return tview.NewTextView().SetText(err.Error()).SetTextAlign(tview.AlignCenter)
 	}
 
 <<<<<<< HEAD
@@ -142,4 +225,38 @@ func pageLoader(ctx context.Context, tviewApp *tview.Application, walletMiddlewa
 	}
 
 	return pages
+}
+
+func syncBlockchain(wallet app.WalletMiddleware) {
+
+	err := wallet.SyncBlockChain(&app.BlockChainSyncListener{
+		SyncStarted: func() {
+			updateStatus("Blockchain sync started...", walletcore.SyncStatusInProgress)
+		},
+		SyncEnded: func(err error) {
+			if err != nil {
+				updateStatus(fmt.Sprintf("Blockchain sync completed with error: %s", err.Error()), walletcore.SyncStatusError)
+			} else {
+				updateStatus("Blockchain sync completed successfully", walletcore.SyncStatusSuccess)
+			}
+		},
+		OnHeadersFetched: func(percentageProgress int64) {
+			updateStatus(fmt.Sprintf("Blockchain sync in progress. Fetching headers (1/3): %d%%", percentageProgress), walletcore.SyncStatusInProgress)
+		},
+		OnDiscoveredAddress: func(_ string) {
+			updateStatus("Blockchain sync in progress. Discovering addresses (2/3)", walletcore.SyncStatusInProgress)
+		},
+		OnRescanningBlocks: func(percentageProgress int64) {
+			updateStatus(fmt.Sprintf("Blockchain sync in progress. Rescanning blocks (3/3): %d%%", percentageProgress), walletcore.SyncStatusInProgress)
+		},
+	}, false)
+
+	if err != nil {
+		updateStatus(fmt.Sprintf("Blockchain sync failed to start. %s", err.Error()), walletcore.SyncStatusError)
+	}
+}
+
+func updateStatus(report string, status walletcore.SyncStatus) {
+	Report = report
+	Status = status
 }
