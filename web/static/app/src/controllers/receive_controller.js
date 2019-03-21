46b28f1b7c270d87557cfb69464f501485e9df5c
Ademu Anthony <ademuanthony@gmail.com>
2019-03-21 14:25:01 +0100

    rewrite embedded script as independent stimulsjs controller

---
+++ web/static/app/src/controllers/receive_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from 'stimulus'
+import axios from 'axios'
+
+export default class extends Controller {
+  static get targets () {
+    return [
+        'account', 'address', 'image'
+    ]
+  }
+
+  generate (evn) {
+    evn.preventDefault()
+    let _this = this
+    axios.get('/generate-address/' + this.accountTarget.value)
+        .then((response) => {
+          let result = response.data
+          if (result.success) {
+            _this.addressTarget.textContent = result.address
+            _this.imageTarget.setAttribute('src', result.imageData)
+          } else {
+            window.alert(result.message)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          _this.setErrorMessage('A server error occurred')
+        })
+  }
+}
