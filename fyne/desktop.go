7f627dab27bb59a4aaae28de9554a0be1bfbe92c
Matthew Reichardt <me@matthewreichardt.com>
2019-03-23 22:08:03 +0900

    Stubbed out very basic fyne gui

--- fyne/desktop.go
+++ fyne/desktop.go
@@ -1,20 +1,48 @@
 package fyne
 
 import (
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 )
 
 func LaunchApp() {
 	a := app.New()
+	w := a.NewWindow("GoDCR Wallet")
 
-	w := a.NewWindow("Hello")
-	w.FixedSize()
-	w.SetContent(widget.NewVBox(
-		widget.NewLabel("Hello Fyne!"),
-		widget.NewButton("Quit", func() {
-			a.Quit()
-		}),
+	content := widget.NewVBox(
+	)
+
+	menu := widget.NewVBox(
+		widget.NewGroup("GoDCR", widget.NewVBox(
+			widget.NewButton("Dashboard", func() {
+
+			}),
+			widget.NewButton("Accounts", func() {
+
+			}),
+			widget.NewButton("Send", func() {
+
+			}),
+			widget.NewButton("Recieve", func() {
+
+			}),
+			widget.NewButton("Quit", func() {
+				a.Quit()
+			}),
+		)),
+	)
+
+	w.SetContent(fyne.NewContainerWithLayout(layout.NewGridLayout(1),
+		fyne.NewContainerWithLayout(layout.NewGridLayout(4),
+			fyne.NewContainerWithLayout(layout.NewGridLayout(1),
+				menu,
+			),
+			fyne.NewContainerWithLayout(layout.NewGridLayout(8),
+				content,
+			),
+		),
 	))
 
 	w.ShowAndRun()
