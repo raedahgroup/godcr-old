78936652536a245e04e17c71a2d626dd1df09378
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2019-04-06 05:09:58 +0100

    format tx details inputs and outputs amount

--- terminal/pages/history.go
+++ terminal/pages/history.go
@@ -10,6 +10,8 @@ import (
 	"github.com/raedahgroup/godcr/app/walletcore"
 	"github.com/raedahgroup/godcr/terminal/primitives"
 	"github.com/rivo/tview"
+	"math"
+	"strings"
 )
 
 func historyPage(wallet walletcore.Wallet, setFocus func(p tview.Primitive) *tview.Application, clearFocus func()) tview.Primitive {
@@ -38,6 +40,7 @@ func historyPage(wallet walletcore.Wallet, setFocus func(p tview.Primitive) *tvi
 		hintText.SetText("(TIP: Use ARROW UP/DOWN to select txn, ENTER to view details, ESC to return to nav menu)")
 
 		body.AddItem(historyTable, 0, 1, true)
+		setFocus(historyTable)
 	}
 
 	errorTextView := primitives.WordWrappedTextView("")
@@ -89,7 +92,7 @@ func historyPage(wallet walletcore.Wallet, setFocus func(p tview.Primitive) *tvi
 		return tview.NewTableCell(text).SetAlign(tview.AlignCenter).SetSelectable(false)
 	}
 
-	// Histroy Table header
+	// history table header
 	historyTable.SetCell(0, 0, tableHeaderCell("#"))
 	historyTable.SetCell(0, 1, tableHeaderCell("Date"))
 	historyTable.SetCell(0, 4, tableHeaderCell("Direction"))
@@ -164,11 +167,48 @@ func fetchTransactionDetail(txHash string, wallet walletcore.Wallet, displayErro
 	transactionDetailsTable.SetCellSimple(6, 1, tx.Direction.String())
 	transactionDetailsTable.SetCellSimple(7, 1, tx.Fee)
 	transactionDetailsTable.SetCellSimple(8, 1, fmt.Sprintf("%s/kB", tx.FeeRate))
-	
+
+	decimalPlaces := func(n float64) string {
+		decimalPlaces := fmt.Sprintf("%f", n - math.Floor(n))
+		decimalPlaces = strings.Replace(decimalPlaces, "0", "", -1)
+		decimalPlaces = strings.Replace(decimalPlaces, ".", "", -1)
+		return decimalPlaces
+	}
+
+	// calculate max number of digits after decimal point for inputs and outputs
+	maxDecimalPlaces := 0
+	for _, txIn := range tx.Inputs {
+		decimalPlaces := decimalPlaces(dcrutil.Amount(txIn.AmountIn).ToCoin())
+		nDecimalPlaces := len(decimalPlaces)
+		if nDecimalPlaces > maxDecimalPlaces {
+			maxDecimalPlaces = nDecimalPlaces
+		}
+	}
+	for _, txOut := range tx.Outputs {
+		decimalPlaces := decimalPlaces(dcrutil.Amount(txOut.Value).ToCoin())
+		nDecimalPlaces := len(decimalPlaces)
+		if nDecimalPlaces > maxDecimalPlaces {
+			maxDecimalPlaces = nDecimalPlaces
+		}
+	}
+
+	formatAmount := func(amount int64) string {
+		dcrAmount := dcrutil.Amount(amount).ToCoin()
+		wholeNumber := int(math.Floor(dcrAmount))
+		decimalPlaces := decimalPlaces(dcrAmount)
+
+		if len(decimalPlaces) == 0 {
+			//decimalPlaces = "0"
+			return fmt.Sprintf("%d%-*s DCR", wholeNumber, maxDecimalPlaces + 1, decimalPlaces)
+		}
+
+		return fmt.Sprintf("%d.%-*s DCR", wholeNumber, maxDecimalPlaces, decimalPlaces)
+	}
+
 	transactionDetailsTable.SetCellSimple(9, 0, "-Inputs-")
 	for _, txIn := range tx.Inputs {
 		row := transactionDetailsTable.GetRowCount()
-		transactionDetailsTable.SetCell(row, 0, tview.NewTableCell(dcrutil.Amount(txIn.AmountIn).String()).SetAlign(tview.AlignRight))
+		transactionDetailsTable.SetCell(row, 0, tview.NewTableCell(formatAmount(txIn.AmountIn)).SetAlign(tview.AlignRight))
 		transactionDetailsTable.SetCellSimple(row, 1, txIn.PreviousOutpoint)
 	}
 
@@ -181,7 +221,7 @@ func fetchTransactionDetail(txHash string, wallet walletcore.Wallet, displayErro
 			continue
 		}
 
-		outputAmount := dcrutil.Amount(txOut.Value).String()
+		outputAmount := formatAmount(txOut.Value)
 		for _, address := range txOut.Addresses {
 			accountName := address.AccountName
 			if !address.IsMine {
