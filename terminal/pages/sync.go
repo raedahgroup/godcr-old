ce06b8f635075aca1eead3b7c79b7bda4352fe3d
edono morrison <edonomorrison@gmail.com>
2019-04-07 01:30:17 +0100

    uncomment

--- terminal/pages/sync.go
+++ terminal/pages/sync.go
@@ -3,58 +3,58 @@ package pages
 import (
 	"fmt"
 
-	// "github.com/gdamore/tcell"
+	"github.com/gdamore/tcell"
 	"github.com/raedahgroup/godcr/app"
-	// "github.com/raedahgroup/godcr/terminal/helpers"
-	// "github.com/raedahgroup/godcr/terminal/primitives"
+	"github.com/raedahgroup/godcr/terminal/helpers"
+	"github.com/raedahgroup/godcr/terminal/primitives"
 	"github.com/rivo/tview"
 )
 
 func LaunchSyncPage(tviewApp *tview.Application, walletMiddleware app.WalletMiddleware) {
-	// body := tview.NewFlex().SetDirection(tview.FlexRow)
+	body := tview.NewFlex().SetDirection(tview.FlexRow)
 
-	// // page title and hint
-	// body.AddItem(primitives.NewCenterAlignedTextView("Synchronizing"), 2, 0, false)
-	// hintText := primitives.WordWrappedTextView("(Press Enter or Esc to cancel sync and exit the app)")
-	// hintText.SetTextColor(helpers.HintTextColor)
-	// body.AddItem(hintText, 3, 0, false)
+	// page title and hint
+	body.AddItem(primitives.NewCenterAlignedTextView("Synchronizing"), 2, 0, false)
+	hintText := primitives.WordWrappedTextView("(Press Enter or Esc to cancel sync and exit the app)")
+	hintText.SetTextColor(helpers.HintTextColor)
+	body.AddItem(hintText, 3, 0, false)
 
-	// // text view to show sync progress updates
-	// syncStatusTextView := primitives.NewCenterAlignedTextView("")
-	// body.AddItem(syncStatusTextView, 3, 0, false)
+	// text view to show sync progress updates
+	syncStatusTextView := primitives.NewCenterAlignedTextView("")
+	body.AddItem(syncStatusTextView, 3, 0, false)
 
-	// cancelButton := tview.NewButton("Cancel and Exit")
-	// body.AddItem(cancelButton, 1, 0, true)
+	cancelButton := tview.NewButton("Cancel and Exit")
+	body.AddItem(cancelButton, 1, 0, true)
 
-	// cancelAndExit := func() {
-	// 	tviewApp.Stop()
-	// }
-	// cancelButton.SetSelectedFunc(cancelAndExit)
-	// body.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-	// 	if event.Key() == tcell.KeyEsc {
-	// 		cancelAndExit()
-	// 		return nil
-	// 	}
-	// 	return event
-	// })
+	cancelAndExit := func() {
+		tviewApp.Stop()
+	}
+	cancelButton.SetSelectedFunc(cancelAndExit)
+	body.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyEsc {
+			cancelAndExit()
+			return nil
+		}
+		return event
+	})
 
-	// tviewApp.SetRoot(body, true)
+	tviewApp.SetRoot(body, true)
 
-	// // function to update the sync page with status report from the sync operation
-	// updateStatus := func(status string) {
-	// 	tviewApp.QueueUpdateDraw(func() {
-	// 		syncStatusTextView.SetText(status)
-	// 	})
-	// }
+	// function to update the sync page with status report from the sync operation
+	updateStatus := func(status string) {
+		tviewApp.QueueUpdateDraw(func() {
+			syncStatusTextView.SetText(status)
+		})
+	}
 
-	// // function to be executed after the sync operation completes successfully
-	// afterSyncing := func() {
-	// 	tviewApp.QueueUpdateDraw(func() {
-	tviewApp.SetRoot(rootPage(tviewApp, walletMiddleware), true)
-	// 	})
-	// }
+	// function to be executed after the sync operation completes successfully
+	afterSyncing := func() {
+		tviewApp.QueueUpdateDraw(func() {
+			tviewApp.SetRoot(rootPage(tviewApp, walletMiddleware), true)
+		})
+	}
 
-	// startSync(walletMiddleware, updateStatus, afterSyncing)
+	startSync(walletMiddleware, updateStatus, afterSyncing)
 }
 
 func startSync(walletMiddleware app.WalletMiddleware, updateStatus func(string), afterSyncing func()) {
