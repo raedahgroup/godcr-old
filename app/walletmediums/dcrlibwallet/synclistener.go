8da339524d61c9cb45b8daf980571f6d8915a8d3
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2019-04-07 22:11:35 +0100

    todo message

--- app/walletmediums/dcrlibwallet/synclistener.go
+++ app/walletmediums/dcrlibwallet/synclistener.go
@@ -250,6 +250,7 @@ func (listener *syncListener) OnSynced(synced bool) {
 	listener.syncInfoUpdated(listener.privateSyncInfo)
 }
 
+// todo sync hasn't ended
 func (listener *syncListener) OnSyncError(code int, err error) {
 	if !listener.syncing {
 		// ignore subsequent updates
