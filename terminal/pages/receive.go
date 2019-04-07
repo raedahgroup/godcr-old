740e604b98bd64190a0badbe3a7186c069f8efe8
edono morrison <edonomorrison@gmail.com>
2019-04-07 03:25:01 +0100

    word casing

--- terminal/pages/receive.go
+++ terminal/pages/receive.go
@@ -15,7 +15,7 @@ func receivePage(wallet walletcore.Wallet, hintTextView *primitives.TextView, se
 	body := tview.NewFlex().SetDirection(tview.FlexRow)
 	form := tview.NewForm()
 
-	body.AddItem(primitives.NewLeftAlignedTextView("GENERATE RECEIVE ADDRESS"), 2, 1, false)
+	body.AddItem(primitives.NewLeftAlignedTextView("Generate Receive Address"), 2, 1, false)
 
 	accounts, err := wallet.AccountsOverview(walletcore.DefaultRequiredConfirmations)
 	if err != nil {
@@ -71,7 +71,7 @@ func receivePage(wallet walletcore.Wallet, hintTextView *primitives.TextView, se
 		}
 	}
 
-	hintTextView.SetText("TIP: Navigate with TAB and SHIFT+TAB, hit ENTER to generate Address. ESC to return to Navigation menu")
+	hintTextView.SetText("TIP: Navigate with TAB and SHIFT+TAB, hit ENTER to generate Address. ESC to return to navigation menu")
 
 	setFocus(body)
 	return body
