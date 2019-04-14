d156ecbd983848fb40be441d68965eff232fe113
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2019-04-14 05:59:06 +0100

    refactor receive page

--- terminal/pages/receive.go
+++ terminal/pages/receive.go
@@ -14,32 +14,47 @@ import (
 func receivePage(wallet walletcore.Wallet, hintTextView *primitives.TextView, setFocus func(p tview.Primitive) *tview.Application, clearFocus func()) tview.Primitive {
 	body := tview.NewFlex().SetDirection(tview.FlexRow)
 
-	body.AddItem(primitives.NewLeftAlignedTextView("Receiving Decred"), 2, 1, false)
-	body.AddItem(primitives.NewLeftAlignedTextView("Each time you request a payment, a new address is created to protect your privacy.").SetTextColor(helpers.HintTextColor), 3, 1, false)
+	body.AddItem(primitives.NewLeftAlignedTextView("Receiving Decred"), 1, 1, false)
+	receivingDecredHintTextView := primitives.NewLeftAlignedTextView(walletcore.ReceivingDecredHint).
+		SetTextColor(helpers.HintTextColor)
+	body.AddItem(receivingDecredHintTextView, 3, 1, false)
 
 	accounts, err := wallet.AccountsOverview(walletcore.DefaultRequiredConfirmations)
 	if err != nil {
 		return body.AddItem(primitives.NewLeftAlignedTextView(fmt.Sprintf("Error: %s", err.Error())), 0, 1, false)
 	}
 
-	outputMessageTextView := primitives.WordWrappedTextView("")
-	outputMessageTextView.SetTextColor(helpers.DecredOrangeColor)
+	errorMessageTextView := primitives.WordWrappedTextView("").
+		SetTextColor(helpers.DecredOrangeColor)
 
 	displayErrorMessage := func(message string) {
-		body.RemoveItem(outputMessageTextView)
-		outputMessageTextView.SetText(message)
-		body.AddItem(outputMessageTextView, 2, 0, false)
+		body.RemoveItem(errorMessageTextView)
+		errorMessageTextView.SetText(message)
+		body.AddItem(errorMessageTextView, 2, 0, false)
 	}
 
 	qrCodeTextView := primitives.NewCenterAlignedTextView("")
-	addressTextView := primitives.NewCenterAlignedTextView("")
-	addressTextView.SetTextColor(helpers.DecredLightBlueColor)
+	addressTextView := primitives.NewCenterAlignedTextView("").
+		SetTextColor(helpers.DecredLightBlueColor)
 
-	displayOutput := func(qrCode, address string) {
+	generateAndDisplayAddress := func(accountNumber uint32) {
+		// clear previously generated address or displayed error before generating new one
 		body.RemoveItem(qrCodeTextView)
 		body.RemoveItem(addressTextView)
-		body.AddItem(qrCodeTextView.SetText(qrCode), 19, 0, true)
-		body.AddItem(addressTextView.SetText(address), 0, 1, true)
+		body.RemoveItem(errorMessageTextView)
+
+		address, qr, err := generateAddressAndQrCode(wallet, accountNumber)
+		if err != nil {
+			errorText := fmt.Sprintf("Error: %s", err.Error())
+			displayErrorMessage(errorText)
+			return
+		}
+
+		qrCodeTextView.SetText(qr.ToSmallString(false))
+		addressTextView.SetText(address)
+
+		body.AddItem(qrCodeTextView, 19, 0, true)
+		body.AddItem(addressTextView, 0, 1, true)
 	}
 
 	accountNumbers := make([]uint32, len(accounts))
@@ -50,29 +65,17 @@ func receivePage(wallet walletcore.Wallet, hintTextView *primitives.TextView, se
 	}
 
 	if len(accounts) == 1 {
-		address, qr, err := generateAddress(wallet, accounts[0].Number)
-		if err != nil {
-			errorText := fmt.Sprintf("Error: %s", err.Error())
-			displayErrorMessage(errorText)
-			return nil
-		}
-
-		body.AddItem(primitives.NewLeftAlignedTextView(fmt.Sprintf("Source Account: %s", accounts[0].Name)).SetTextColor(helpers.DecredLightBlueColor).SetDoneFunc(func(key tcell.Key) {
+		singleAccountTextView := primitives.NewLeftAlignedTextView(fmt.Sprintf("Source Account: %s", accounts[0].Name))
+		singleAccountTextView.SetTextColor(helpers.DecredLightBlueColor)
+		singleAccountTextView.SetDoneFunc(func(key tcell.Key) {
 			if key == tcell.KeyEscape {
 				clearFocus()
 			}
-		}), 2, 1, true)
+		})
 
-		displayOutput(qr.ToSmallString(false), address)
+		body.AddItem(singleAccountTextView, 2, 1, true)
 		hintTextView.SetText("TIP: ESC to return to navigation menu")
 	} else {
-		// receive form
-		form := primitives.NewForm(false)
-		form.SetBorderPadding(0, 0, 0, 0)
-		form.SetHorizontal(true).
-			SetLabelColor(helpers.DecredLightBlueColor)
-		body.AddItem(form, 2, 0, true)
-
 		accountNumbers := make([]uint32, len(accounts))
 		accountNames := make([]string, len(accounts))
 		for index, account := range accounts {
@@ -80,28 +83,28 @@ func receivePage(wallet walletcore.Wallet, hintTextView *primitives.TextView, se
 			accountNumbers[index] = account.Number
 		}
 
+		form := primitives.NewForm(false)
+		form.SetBorderPadding(0, 0, 0, 0)
+		form.SetLabelColor(helpers.DecredLightBlueColor)
 		form.AddDropDown("Source Account: ", accountNames, 0, func(option string, optionIndex int) {
 			accountNumber := accountNumbers[optionIndex]
-
-			address, qr, err := generateAddress(wallet, accountNumber)
-			if err != nil {
-				errorText := fmt.Sprintf("Error: %s", err.Error())
-				displayErrorMessage(errorText)
-				return
-			}
-
-			displayOutput(qr.ToSmallString(false), address)
-			hintTextView.SetText("TIP: Select Prefered Account and hit ENTER to generate Address. ESC to return to navigation menu")
+			generateAndDisplayAddress(accountNumber)
 		})
 
 		form.SetCancelFunc(clearFocus)
+
+		body.AddItem(form, 2, 0, true)
+		hintTextView.SetText("TIP: Select Prefered Account and hit ENTER to generate Address. ESC to return to navigation menu")
 	}
+
+	// always generate and display address for the first account, even if there are multiple accounts
+	generateAndDisplayAddress(accounts[0].Number)
 
 	setFocus(body)
 	return body
 }
 
-func generateAddress(wallet walletcore.Wallet, accountNumber uint32) (string, *qrcode.QRCode, error) {
+func generateAddressAndQrCode(wallet walletcore.Wallet, accountNumber uint32) (string, *qrcode.QRCode, error) {
 	generatedAddress, err := wallet.ReceiveAddress(accountNumber)
 	if err != nil {
 		return "", nil, err
