caece011d0b2ecc607db25c851fdcb93c3ce3816
edono morrison <edonomorrison@gmail.com>
2019-04-18 22:33:12 +0100

    corrected misplaced code

--- cli/commands/balance.go
+++ cli/commands/balance.go
@@ -19,7 +19,7 @@ func (balanceCommand BalanceCommand) Run(ctx context.Context, wallet walletcore.
 		return err
 	}
 
-	var showAccount, showTotal, showSpendable, showUnconfirmed bool
+	var showAccount, showTotal, showSpendable, showLocked, showUnconfirmed bool
 
 	rows := make([][]interface{}, len(accounts))
 
@@ -31,11 +31,10 @@ func (balanceCommand BalanceCommand) Run(ctx context.Context, wallet walletcore.
 			rows[0] = append(rows[0], accounts[0].Balance.Total)
 		} else {
 			showTotal, showSpendable = true, true
-			rows[0] = append(rows[0], accounts[0].Balance.Total)
-			rows[0] = append(rows[0], accounts[0].Balance.Spendable)
+			rows[0] = append(rows[0], accounts[0].Balance.Total, accounts[0].Balance.Spendable)
 		}
 		if accounts[0].Balance.LockedByTickets != 0 {
-			showSpendable = true
+			showLocked = true
 			rows[0] = append(rows[0], accounts[0].Balance.LockedByTickets)
 		}
 		if accounts[0].Balance.Unconfirmed != 0 {
@@ -51,15 +50,13 @@ func (balanceCommand BalanceCommand) Run(ctx context.Context, wallet walletcore.
 			rows[i] = append(rows[i], account.Name)
 			if account.Balance.Total == account.Balance.Spendable {
 				showTotal = true
-
 				rows[i] = append(rows[i], account.Balance.Total)
 			} else {
 				showTotal, showSpendable = true, true
-				rows[i] = append(rows[i], account.Balance.Total)
-				rows[i] = append(rows[i], account.Balance.Spendable)
+				rows[i] = append(rows[i], accounts[i].Balance.Total, accounts[i].Balance.Spendable)
 			}
 			if account.Balance.LockedByTickets != 0 {
-				showSpendable = true
+				showLocked = true
 				rows[i] = append(rows[i], account.Balance.LockedByTickets)
 			}
 			if account.Balance.Unconfirmed != 0 {
@@ -79,7 +76,7 @@ func (balanceCommand BalanceCommand) Run(ctx context.Context, wallet walletcore.
 	if showSpendable {
 		columns = append(columns, "Spendable")
 	}
-	if showSpendable {
+	if showLocked {
 		columns = append(columns, "Locked")
 	}
 	if showUnconfirmed {
