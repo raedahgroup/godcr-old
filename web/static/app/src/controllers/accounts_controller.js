c5feeef6aeee3ae9e64dd248b644c03c2c192202
Shweini <code.maestro64@gmail.com>
2019-05-09 10:43:49 +0100

    fix indentation issue

--- web/static/app/src/controllers/accounts_controller.js
+++ web/static/app/src/controllers/accounts_controller.js
@@ -3,53 +3,53 @@ import axios from 'axios'
 import { showErrorNotification, showSuccessNotification } from '../utils'
 
 export default class extends Controller {
-    static get targets () {
-        return ['hideAccount', 'defaultAccount']
-    }
+  static get targets () {
+    return ['hideAccount', 'defaultAccount']
+  }
 
-    toggleHideAccount (e) {
-      let accountElement = e.currentTarget
-      let accountNumber = accountElement.getAttribute('data-account')
+  toggleHideAccount (e) {
+    let accountElement = e.currentTarget
+    let accountNumber = accountElement.getAttribute('data-account')
 
-      const postData = (accountElement.checked) ? `hide-account=${accountNumber}` : `reveal-account=${accountNumber}`
-      axios.put('/settings', postData).then((response) => {
-        const result = response.data
-        if (result.success) {
-          showSuccessNotification('Changes saved successfully')
-        } else {
-          showErrorNotification(result.error ? result.error : 'Something went wrong, please try again later')
-          accountElement.checked = !accountElement.checked
-        }
-      }).catch(() => {
+    const postData = (accountElement.checked) ? `hide-account=${accountNumber}` : `reveal-account=${accountNumber}`
+    axios.put('/settings', postData).then((response) => {
+      const result = response.data
+      if (result.success) {
+        showSuccessNotification('Changes saved successfully')
+      } else {
+        showErrorNotification(result.error ? result.error : 'Something went wrong, please try again later')
         accountElement.checked = !accountElement.checked
-        showErrorNotification('A server error occurred')
-      })
-    }
+      }
+    }).catch(() => {
+      accountElement.checked = !accountElement.checked
+      showErrorNotification('A server error occurred')
+    })
+  }
 
-    updateDefaultAccount (e) {
-       const defaultAccountEl = e.currentTarget
-       const defaultAccount = defaultAccountEl.getAttribute('data-account')
+  updateDefaultAccount (e) {
+    const defaultAccountEl = e.currentTarget
+    const defaultAccount = defaultAccountEl.getAttribute('data-account')
 
-       // uncheck all other accounts that were previously marked default
-       this.defaultAccountTargets.forEach((el, i) => {
-           if (el.checked && el.getAttribute('data-account') !== defaultAccount) {
-               el.checked = false
-           }
-       })
+    // uncheck all other accounts that were previously marked default
+    this.defaultAccountTargets.forEach((el, i) => {
+      if (el.checked && el.getAttribute('data-account') !== defaultAccount) {
+        el.checked = false
+      }
+    })
 
-       // post data
-       const postData = `default-account=${defaultAccount}`
-       axios.put('/settings', postData).then((response) => {
-            const result = response.data
-            if (result.success) {
-                showSuccessNotification('Changes saved successfully')
-            } else {
-                showErrorNotification(result.error ? result.error : 'Something went wrong, please try again later')
-                defaultAccountEl.checked = !defaultAccountEl.checked
-            }
-       }).catch(() => {
+    // post data
+    const postData = `default-account=${defaultAccount}`
+    axios.put('/settings', postData).then((response) => {
+      const result = response.data
+      if (result.success) {
+        showSuccessNotification('Changes saved successfully')
+      } else {
+        showErrorNotification(result.error ? result.error : 'Something went wrong, please try again later')
         defaultAccountEl.checked = !defaultAccountEl.checked
-         showErrorNotification('A server error occurred')
-       })
-    }
+      }
+    }).catch(() => {
+      defaultAccountEl.checked = !defaultAccountEl.checked
+      showErrorNotification('A server error occurred')
+    })
+  }
 }
