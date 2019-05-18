9951a830229317c09e07267f4e8b963ea061efda
edono morrison <edonomorrison@gmail.com>
2019-05-18 20:02:16 +0100

    rebased PR

--- web/static/app/src/controllers/history_controller.js
+++ web/static/app/src/controllers/history_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
       'txRowTemplate',
       'errorMessage',
       'previousPageButton', 'pageReport', 'nextPageButton',
-      'loadingIndicator',
+      'loadingIndicator'
     ]
   }
 
