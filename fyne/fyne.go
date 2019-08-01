949ab37613952729c5732dfa0a1b73e54e9ba4b3
Uti Michael <utimichael9@gmail.com>
2019-08-01 10:15:55 +0100

    implemented receive and exit page, updates to realtime stats

--- fyne/fyne.go
+++ fyne/fyne.go
@@ -4,18 +4,18 @@ import (
 	"context"
 
 	"fyne.io/fyne"
-
 	"fyne.io/fyne/app"
 	godcrApp "github.com/raedahgroup/godcr/app"
 	"github.com/raedahgroup/godcr/fyne/pages"
 )
 
+//todo: implement better error logger, probably log to a file
 func LaunchFyne(ctx context.Context, walletMiddleware godcrApp.WalletMiddleware) {
 	fyneApp := app.New()
 	window := fyneApp.NewWindow(godcrApp.DisplayName)
-	sync := pages.ShowSyncWindow(walletMiddleware, window, fyneApp)
+	sync := pages.ShowSyncWindow(ctx, walletMiddleware, window, fyneApp)
+	window.CenterOnScreen()
 	window.Resize(fyne.NewSize(1000, 500))
 	window.SetContent(sync)
-	window.CenterOnScreen()
 	window.ShowAndRun()
 }
--- fyne/pages/menu.go
+++ fyne/pages/menu.go
@@ -1,8 +1,7 @@
 package pages
 
 import (
-	"io/ioutil"
-	"log"
+	"context"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/layout"
@@ -10,17 +9,19 @@ import (
 	godcrApp "github.com/raedahgroup/godcr/app"
 )
 
-var (
+type menuPageData struct {
 	peerConn  *widget.Label
 	blkHeight *widget.Label
+
 	//there might be situations we would want to get the particular opened tab
 	tabs *widget.TabContainer
-)
+}
+
+var menu menuPageData
 
 func init() {
-	peerConn = widget.NewLabel("No peers connected")
-	blkHeight = widget.NewLabel("No blocks fetched")
-
+	menu.peerConn = widget.NewLabel("")
+	menu.blkHeight = widget.NewLabel("")
 }
 
 func pageNotImplemented() fyne.CanvasObject {
@@ -28,53 +29,23 @@ func pageNotImplemented() fyne.CanvasObject {
 	return label
 }
 
-func Menu(wallet godcrApp.WalletMiddleware, app fyne.App, window fyne.Window) fyne.CanvasObject {
-	overviewFile, err := ioutil.ReadFile("./fyne/pages/png/overview.png")
-	if err != nil {
-		log.Fatalln("overview file missing", err)
-	}
-	historyFile, err := ioutil.ReadFile("./fyne/pages/png/block.png")
-	if err != nil {
-		log.Fatalln("history file missing", err)
-	}
-	sendFile, err := ioutil.ReadFile("./fyne/pages/png/send.png")
-	if err != nil {
-		log.Fatalln("send file missing", err)
-	}
-	receiveFile, err := ioutil.ReadFile("./fyne/pages/png/receive.png")
-	if err != nil {
-		log.Fatalln("receive file missing", err)
-	}
-	stakingFile, err := ioutil.ReadFile("./fyne/pages/png/stakeyBaby.png")
-	if err != nil {
-		log.Fatalln("staking file missing", err)
-	}
-	accountsFile, err := ioutil.ReadFile("./fyne/pages/png/wallet.png")
-	if err != nil {
-		log.Fatalln("accounts file missing", err)
-	}
-	securityFile, err := ioutil.ReadFile("./fyne/pages/png/info.png")
-	if err != nil {
-		log.Fatalln("security file missing", err)
-	}
-	settingsFile, err := ioutil.ReadFile("./fyne/pages/png/gears.png")
-	if err != nil {
-		log.Fatalln("settings file missing", err)
-	}
+func menuPage(ctx context.Context, wallet godcrApp.WalletMiddleware, app fyne.App, window fyne.Window) fyne.CanvasObject {
+	menu.tabs = widget.NewTabContainer(
+		widget.NewTabItem("Overview", overviewPage(wallet)),
+		widget.NewTabItem("History", pageNotImplemented()),
+		widget.NewTabItem("Send", pageNotImplemented()),
+		widget.NewTabItem("Receive", receivePage(wallet, window)),
+		widget.NewTabItem("Staking", pageNotImplemented()),
+		widget.NewTabItem("Accounts", pageNotImplemented()),
+		widget.NewTabItem("Security", pageNotImplemented()),
+		widget.NewTabItem("Settings", settingsPage(app)),
+		widget.NewTabItem("Exit", exit(ctx, app, window)))
 
-	tabs = widget.NewTabContainer(
-		widget.NewTabItemWithIcon("Overview", fyne.NewStaticResource("Overview", overviewFile), overviewPage(wallet)),
-		widget.NewTabItemWithIcon("History", fyne.NewStaticResource("History", historyFile), pageNotImplemented()),
-		widget.NewTabItemWithIcon("Send", fyne.NewStaticResource("Send", sendFile), pageNotImplemented()),
-		widget.NewTabItemWithIcon("Receive", fyne.NewStaticResource("Receive", receiveFile), pageNotImplemented()),
-		widget.NewTabItemWithIcon("Staking", fyne.NewStaticResource("Staking", stakingFile), pageNotImplemented()),
-		widget.NewTabItemWithIcon("Accounts", fyne.NewStaticResource("Accounts", accountsFile), pageNotImplemented()),
-		widget.NewTabItemWithIcon("Security", fyne.NewStaticResource("Security", securityFile), pageNotImplemented()),
-		widget.NewTabItemWithIcon("Settings", fyne.NewStaticResource("Settings", settingsFile), settingsPage(app)))
-	tabs.SetTabLocation(widget.TabLocationLeading)
+	menu.tabs.SetTabLocation(widget.TabLocationLeading)
 
 	//where peerConn and blkHeight are the realtime status texts
-	status := widget.NewVBox(peerConn, blkHeight)
-	menu := fyne.NewContainerWithLayout(layout.NewBorderLayout(nil, status, tabs, nil), tabs, status)
-	return menu
+	status := widget.NewVBox(menu.peerConn, menu.blkHeight)
+
+	data := fyne.NewContainerWithLayout(layout.NewBorderLayout(nil, status, menu.tabs, nil), menu.tabs, status)
+	return data
 }
