9aa1d512c0035bae03f6489f9d4fae451c2a3db7
Barisere Jonathan <barry.jonathanks@gmail.com>
2019-08-24 22:49:51 +0100

    Add "add account" functionality.

--- fyne/pages/accounts.go
+++ fyne/pages/accounts.go
@@ -7,7 +7,6 @@ package pages
 import (
 	"fmt"
 	"fyne.io/fyne"
-	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 	"github.com/raedahgroup/godcr/app"
@@ -25,18 +24,82 @@ func accountsPage(wallet app.WalletMiddleware) fyne.CanvasObject {
 		return widget.NewLabel(fmt.Sprintf("Unable to retrieve accounts information: %s", err))
 	}
 	netType := wallet.NetType()
-	pageTitle := widget.NewLabelWithStyle("Accounts", fyne.TextAlignLeading, fyne.TextStyle{Bold:true, Italic:true})
-	pageBox := widget.NewVBox(pageTitle)
+	pageTitle := widget.NewLabelWithStyle("Accounts", fyne.TextAlignLeading, fyne.TextStyle{Bold: true, Italic: true})
+	accountsBox := widget.NewVBox(pageTitle)
 	for _, account := range accounts {
-		pageBox.Append(widgets.NewVSpacer(defaultItemsSpacing))
+		accountsBox.Append(widgets.NewVSpacer(defaultItemsSpacing))
 		hSpace := widgets.NewHSpacer(defaultItemsSpacing)
 		detailSection := widget.NewHBox(hSpace, createDetailSection(account, netType))
 		toggleButton := createToggleButton(detailSection)
 		overviewSection := widget.NewHBox(toggleButton, createOverviewSection(account))
-		pageBox.Append(widget.NewVBox(overviewSection, detailSection))
+		accountsBox.Append(widget.NewVBox(overviewSection, detailSection))
 	}
+	addAccountForm := createAddAccountForm(wallet)
+	addAccountButton := createAddAccountButton(addAccountForm)
 	pageLeftMargin := widgets.NewHSpacer(defaultItemsSpacing)
-	return widget.NewHBox(pageLeftMargin, pageBox)
+	return widget.NewHBox(
+		pageLeftMargin,
+		accountsBox,
+		pageLeftMargin,
+		widget.NewVBox(widget.NewHBox(addAccountButton, layout.NewSpacer()), addAccountForm),
+	)
+}
+
+func toggleCanvasObjectVisibility(target fyne.CanvasObject) {
+	if target.Visible() {
+		target.Hide()
+	} else {
+		target.Show()
+	}
+}
+
+func createAddAccountButton(form fyne.CanvasObject) fyne.CanvasObject {
+	return &widget.Button{
+		Text:  "Add account",
+		Style: widget.PrimaryButton,
+		OnTapped: func() {
+			toggleCanvasObjectVisibility(form)
+		},
+	}
+}
+
+func createAddAccountForm(wallet walletcore.Wallet) fyne.CanvasObject {
+	accountNameEntry := &widget.Entry{
+		PlaceHolder: "Account name",
+	}
+	accountName := &widget.FormItem{
+		Text:   "Account name",
+		Widget: accountNameEntry,
+	}
+	walletPassphraseEntry := &widget.Entry{
+		PlaceHolder: "Wallet passphrase",
+		Password:    true,
+	}
+	walletPassphrase := &widget.FormItem{
+		Text:   "Wallet passphrase",
+		Widget: walletPassphraseEntry,
+	}
+	errorDisplay := widget.NewLabel("")
+	form := &widget.Form{
+		Items: []*widget.FormItem{accountName, walletPassphrase},
+	}
+	resetForm := func() {
+		accountNameEntry.SetText("")
+		walletPassphraseEntry.SetText("")
+		errorDisplay.SetText("")
+		errorDisplay.Hide()
+	}
+	form.OnCancel = resetForm
+	form.OnSubmit = func() {
+		_, err := wallet.NextAccount(accountNameEntry.Text, walletPassphraseEntry.Text)
+		if err != nil {
+			errorDisplay.SetText(fmt.Sprintf("Failed to create account. Reason: %s", err))
+			errorDisplay.Show()
+		} else {
+			resetForm()
+		}
+	}
+	return widget.NewVBox(form, errorDisplay)
 }
 
 func createOverviewSection(account *walletcore.Account) fyne.CanvasObject {
@@ -79,13 +142,8 @@ func createToggleButton(target fyne.CanvasObject) *widget.Button {
 	}
 	setButtonStyle()
 	button.OnTapped = func() {
-		if target.Visible() {
-			target.Hide()
-		} else {
-			target.Show()
-		}
+		toggleCanvasObjectVisibility(target)
 		setButtonStyle()
-		canvas.Refresh(&button)
 	}
 	return &button
 }
