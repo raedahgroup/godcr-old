22562be14f04619faeed61fc54b61b1e01bf13bd
Sirmorrison <Edonomorrison@gmail.com>
2019-09-04 01:41:18 +0100

    Nuklear overview page Improvement (#388)
    * added recent activity table to overview page
    
    * added recent activities title, removed #column
    
    * reverted code
    
    * change Recent Activities header to Recent Activity
    
    * remove mistakenly committed binaries
    
    * remove unnecessary space

--- nuklear/pagehandlers/overview.go
+++ nuklear/pagehandlers/overview.go
@@ -2,6 +2,7 @@ package pagehandlers
 
 import (
 	"github.com/aarzilli/nucular"
+	"github.com/decred/dcrd/dcrutil"
 	"github.com/raedahgroup/godcr/app/config"
 	"github.com/raedahgroup/godcr/app/walletcore"
 	"github.com/raedahgroup/godcr/nuklear/styles"
@@ -11,9 +12,11 @@ import (
 type OverviewHandler struct {
 	err      error
 	accounts []*walletcore.Account
+	wallet   walletcore.Wallet
 }
 
 func (handler *OverviewHandler) BeforeRender(wallet walletcore.Wallet, settings *config.Settings, _ func()) bool {
+	handler.wallet = wallet
 	handler.accounts, handler.err = wallet.AccountsOverview(walletcore.DefaultRequiredConfirmations)
 	return true
 }
@@ -26,6 +29,48 @@ func (handler *OverviewHandler) Render(window *nucular.Window) {
 			contentWindow.DisplayErrorMessage("Error fetching accounts balance", handler.err)
 		} else {
 			contentWindow.AddLabel(walletcore.WalletBalance(handler.accounts), widgets.LeftCenterAlign)
+			contentWindow.AddHorizontalSpace(20)
+			handler.displayRecentActivities(contentWindow)
 		}
 	})
+}
+
+func (handler *OverviewHandler) displayRecentActivities(contentWindow *widgets.Window) {
+	contentWindow.AddLabelWithFont("Recent Activity", widgets.LeftCenterAlign, styles.BoldPageContentFont)
+
+	txns, err := handler.wallet.TransactionHistory(0, 5, nil)
+	if err != nil {
+		handler.err = err
+	}
+
+	if len(txns) == 0 {
+		contentWindow.AddHorizontalSpace(20)
+		contentWindow.AddLabel("No Transaction yet", widgets.CenterAlign)
+		return
+	}
+
+	historyTable := widgets.NewTable()
+
+	// render table header with nav font
+	historyTable.AddRowWithFont(styles.NavFont,
+		widgets.NewLabelTableCell("Date", "LC"),
+		widgets.NewLabelTableCell("Direction", "LC"),
+		widgets.NewLabelTableCell("Amount", "LC"),
+		widgets.NewLabelTableCell("Fee", "LC"),
+		widgets.NewLabelTableCell("Type", "LC"),
+		widgets.NewLabelTableCell("Hash", "LC"),
+	)
+
+	for _, tx := range txns {
+		historyTable.AddRow(
+			widgets.NewLabelTableCell(tx.ShortTime, "LC"),
+			widgets.NewLabelTableCell(tx.Direction.String(), "LC"),
+			widgets.NewLabelTableCell(dcrutil.Amount(tx.Amount).String(), "RC"),
+			widgets.NewLabelTableCell(dcrutil.Amount(tx.Fee).String(), "RC"),
+			widgets.NewLabelTableCell(tx.Type, "LC"),
+			widgets.NewLabelTableCell(tx.Hash, "LC"),
+		)
+	}
+
+	historyTable.Render(contentWindow)
 }
