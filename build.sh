08d370c63a156fa59cd6e8cf59f4d40f4305cdfc
Ademu <ademuanthony@gmail.com>
2019-09-20 10:23:49 +0100

    simply web build and binary to originating package after build

--- build.sh
+++ build.sh
@@ -1,45 +1,41 @@
 #!/usr/bin/env bash
-function buildWebFrontEnd {
-  echo "cd web/static/app"
-  cd web/static/app
-  echo 'yarn install'
-  yarn install
-  echo 'yarn build'
-  yarn build
-  echo 'cd ../../../'
-  cd ../../../
-}
-
 function deployWeb() {
-  buildWebFrontEnd
-  cd cmd/godcr-web
-  echo 'go mod download'
-  go mod download
-  echo 'packr2 build'
-  packr2 build
-  cd ../../
+   echo "building frontend assets with yarn"
+   (cd web/static/app && yarn install && yarn build)
+   echo "building godcr-web binary with packr2"
+   (cd cmd/godcr-web && packr2 build && cd ../../)
+   mv ./cmd/godcr-web/godcr-web ./godcr-web
+   echo "binary saved to ./godcr-web"
 }
 
 function buildFyne() {
     echo "packing assets with packr2"
     (cd fyne && packr2)
     echo "building with go build"
-    cd ../ && go build ./cmd/godcr-fyne
+    cd cmd/godcr-fyne && go build && cd ../../
+   mv ./cmd/godcr-web/godcr-fyne ./godcr-fyne
+   echo "binary saved to ./godcr-fyne"
 }
 
 function buildNuklear() {
     echo "building with go build"
-    cd ./cmd/godcr-nuklear && go build
+    cd ./cmd/godcr-nuklear && go build && cd ../../
+   mv ./cmd/godcr-web/godcr-nuklear ./godcr-nuklear
+   echo "binary saved to ./godcr-nuklear"
 }
 
 function buildTerminal() {
     echo "building with go build"
-    cd ./cmd/godcr-terminal && go build
+    cd ./cmd/godcr-terminal && go build && cd ../../
+   mv ./cmd/godcr-web/godcr-terminal ./godcr-terminal
+   echo "binary saved to ./godcr-terminal"
 }
 
 function buildCli() {
     echo "building with go build"
-    cd ../ && go build ./cmd/godcr-cli
+    cd ../ && go build ./cmd/godcr-cli && cd ../../
+   mv ./cmd/godcr-web/godcr-cli ./godcr-cli
+   echo "binary saved to ./godcr-cli"
 }
 
 interface=$1
