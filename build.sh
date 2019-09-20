61e1dca9a3801e5e292ee7922c9dd8b05de64c00
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2019-09-20 10:28:59 +0100

    make build.sh executable, correct build commands

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 function deployWeb() {
    echo "building frontend assets with yarn"
-   (cd web/static/app && yarn install && yarn build)
-   echo "building godcr-web binary with packr2"
-   (cd cmd/godcr-web && packr2 build && cd ../../)
+   (cd ./web/static/app && yarn install && yarn build)
+   echo "building godcr-web with packr2"
+   (cd ./cmd/godcr-web && packr2 build)
    mv ./cmd/godcr-web/godcr-web ./godcr-web
    echo "binary saved to ./godcr-web"
 }
@@ -11,44 +11,44 @@ function deployWeb() {
 function buildFyne() {
     echo "packing assets with packr2"
     (cd fyne && packr2)
-    echo "building with go build"
-    cd cmd/godcr-fyne && go build && cd ../../
-   mv ./cmd/godcr-web/godcr-fyne ./godcr-fyne
-   echo "binary saved to ./godcr-fyne"
+    echo "building godcr-fyne with go build"
+    (cd ./cmd/godcr-fyne && go build)
+    mv ./cmd/godcr-fyne/godcr-fyne ./godcr-fyne
+    echo "binary saved to ./godcr-fyne"
 }
 
 function buildNuklear() {
     echo "building with go build"
-    cd ./cmd/godcr-nuklear && go build && cd ../../
-   mv ./cmd/godcr-web/godcr-nuklear ./godcr-nuklear
-   echo "binary saved to ./godcr-nuklear"
+    (cd ./cmd/godcr-nuklear && go build)
+    mv ./cmd/godcr-nuklear/godcr-nuklear ./godcr-nuklear
+    echo "binary saved to ./godcr-nuklear"
 }
 
 function buildTerminal() {
     echo "building with go build"
-    cd ./cmd/godcr-terminal && go build && cd ../../
-   mv ./cmd/godcr-web/godcr-terminal ./godcr-terminal
-   echo "binary saved to ./godcr-terminal"
+    (cd ./cmd/godcr-terminal && go build)
+    mv ./cmd/godcr-terminal/godcr-terminal ./godcr-terminal
+    echo "binary saved to ./godcr-terminal"
 }
 
 function buildCli() {
     echo "building with go build"
-    cd ../ && go build ./cmd/godcr-cli && cd ../../
-   mv ./cmd/godcr-web/godcr-cli ./godcr-cli
-   echo "binary saved to ./godcr-cli"
+    (cd ./cmd/godcr-cli && go build)
+    mv ./cmd/godcr-cli/godcr-cli ./godcr-cli
+    echo "binary saved to ./godcr-cli"
 }
 
 interface=$1
 if [[ "$interface" = "web" ]]; then
-  deployWeb
+    deployWeb
 elif [[ "$interface" = "fyne" ]]; then
-  buildFyne
+    buildFyne
 elif [[ "$interface" = "nuklear" ]]; then
-  buildNuklear
+    buildNuklear
 elif [[ "$interface" = "terminal" ]]; then
-  buildTerminal
+    buildTerminal
 elif [[ "$interface" = "cli" ]]; then
-  buildCli
+    buildCli
 else
     echo "Usage: ./build.sh {interface} e.g. ./build.sh web"
 fi
