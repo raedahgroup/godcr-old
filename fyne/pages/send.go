b8800a4a5cb8e227515d5335816562c2fd5accde
metaclips <utimichael9@gmail.com>
2019-11-05 22:49:26 +0100

    created dynamic data struct

--- fyne/pages/send.go
+++ fyne/pages/send.go
@@ -5,6 +5,11 @@ import (
 	"fyne.io/fyne/widget"
 )
 
+type sendPageDynamicData struct {
+	fromAccountSelect *widget.Select
+	toAccountSelect   *widget.Select
+}
+
 func sendPageContent() fyne.CanvasObject {
 	return widget.NewLabelWithStyle("Send", fyne.TextAlignLeading, fyne.TextStyle{Bold: true, Italic: true})
 }
