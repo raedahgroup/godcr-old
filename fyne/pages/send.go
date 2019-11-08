713e554ae991c3be9f8f018e7e21378dfb9c5ecc
metaclips <utimichael9@gmail.com>
2019-11-08 17:46:10 +0100

    fixed alignment in account dropdown. implemented transaction notifier to send page

--- fyne/pages/send.go
+++ fyne/pages/send.go
@@ -16,15 +16,29 @@ import (
 )
 
 type sendPageDynamicData struct {
-	fromAccountSelect *widget.Box
-	toAccountSelect   *widget.Box
-	errorLabel        *widget.Label
+	// houses all clickable box
+	receivingAccount *widget.Box
+	sendingAccount   *widget.Box
+
+	errorLabel                           *widget.Label
+	sendingSelectedAccountLabel          *widget.Label
+	sendingSelectedAccountBalanceLabel   *widget.Label
+	receivingSelectedAccountLabel        *widget.Label
+	receivingSelectedAccountBalanceLabel *widget.Label
 }
 
 var sendPage sendPageDynamicData
 
 func sendPageContent(dcrlw *dcrlibwallet.LibWallet) fyne.CanvasObject {
-	icons, err := assets.GetIcons(assets.InfoIcon, assets.MoreIcon)
+	// acct, _ := dcrlw.AccountNumber("default")
+	// txauthor := dcrlw.NewUnsignedTx(int32(acct), 0)
+	// txauthor.AddSendDestination("TsfDLrRkk9ciUuwfp2b8PawwnukYD7yAjGd", dcrlibwallet.AmountAtom(10), false)
+	// fmt.Println(txauthor.EstimateMaxSendAmount())
+	// hash, err := txauthor.Broadcast([]byte("admin"))
+	// fmt.Println(hash, err)
+	// fmt.Println(chainhash.NewHash(hash))
+
+	icons, err := assets.GetIcons(assets.InfoIcon, assets.MoreIcon, assets.ReceiveAccountIcon, assets.CollapseIcon)
 	if err != nil {
 		return widget.NewLabelWithStyle(err.Error(), fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
 	}
@@ -40,13 +54,57 @@ func sendPageContent(dcrlw *dcrlibwallet.LibWallet) fyne.CanvasObject {
 
 	baseWidgets := widget.NewHBox(sendLabel, layout.NewSpacer(), clickabelInfoIcon, clickabelMoreIcon)
 
-	// fromAccountSelectionBox := widget.NewGroup("From")
-	// ToAccountSelectionBox := widget.NewGroup("To")
+	accounts, err := dcrlw.GetAccountsRaw(dcrlibwallet.DefaultRequiredConfirmations)
+	if err != nil {
+		return widget.NewLabelWithStyle("could not retrieve account, "+err.Error(), fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
+	}
 
-	return widget.NewHBox(widgets.NewHSpacer(10), widget.NewVBox(baseWidgets, widget.NewLabel("Hello")))
+	sendPage.receivingSelectedAccountLabel = widget.NewLabel(accounts.Acc[0].Name)
+	sendPage.receivingSelectedAccountBalanceLabel = widget.NewLabel(dcrutil.Amount(accounts.Acc[0].TotalBalance).String())
+	sendPage.receivingAccount = widget.NewVBox()
+
+	receivingAccountBox := widget.NewHBox(
+		widgets.NewHSpacer(15),
+		widget.NewIcon(icons[assets.ReceiveAccountIcon]),
+		widgets.NewHSpacer(20),
+		sendPage.receivingSelectedAccountLabel,
+		widgets.NewHSpacer(30),
+		sendPage.receivingSelectedAccountBalanceLabel,
+		widgets.NewHSpacer(8),
+		widget.NewIcon(icons[assets.CollapseIcon]),
+	)
+
+	receivingAccountSelectionPopup := widget.NewPopUp(sendPage.receivingAccount, fyne.CurrentApp().Driver().AllWindows()[0].Canvas())
+	getAccountInBox(sendPage.receivingAccount, sendPage.receivingSelectedAccountLabel, sendPage.receivingSelectedAccountBalanceLabel,
+		accounts, icons[assets.ReceiveAccountIcon], receivingAccountSelectionPopup)
+	receivingAccountSelectionPopup.Hide()
+
+	var receivingAccountClickableBox *widgets.ClickableBox
+	receivingAccountClickableBox = widgets.NewClickableBox(receivingAccountBox, func() {
+		receivingAccountSelectionPopup.Move(fyne.CurrentApp().Driver().AbsolutePositionForObject(
+			receivingAccountClickableBox).Add(fyne.NewPos(0, receivingAccountClickableBox.Size().Height)))
+		receivingAccountSelectionPopup.Show()
+	})
+
+	// // make receivingAccountTab a clickable box thereby showing the popup
+
+	receivingAccountGroup := widget.NewGroup("To", receivingAccountClickableBox)
+
+	// sendingAccountsDropdown := widgets.NewClickableBox(receivingAccountTab, func() {
+
+	// })
+
+	// var accountDropdown *widgets.ClickableBox
+	// accountDropdown = widgets.NewClickableBox(accountTab, func() {
+	// 	accountSelectionPopup.Move(fyne.CurrentApp().Driver().AbsolutePositionForObject(
+	// 		accountDropdown).Add(fyne.NewPos(0, accountDropdown.Size().Height)))
+	// 	accountSelectionPopup.Show()
+	// })
+
+	return widget.NewHBox(widgets.NewHSpacer(10), widget.NewVBox(baseWidgets, receivingAccountGroup))
 }
 
-func getAccountInBox(accountListWidget *widget.Box, accounts *dcrlibwallet.Accounts, receiveIcon fyne.Resource, popup *widget.PopUp) {
+func getAccountInBox(dropdownContent *widget.Box, selectedAccountLabel *widget.Label, selectedAccountBalanceLabel *widget.Label, accounts *dcrlibwallet.Accounts, receiveIcon fyne.Resource, popup *widget.PopUp) {
 	for index, account := range accounts.Acc {
 		if account.Name == "imported" {
 			continue
@@ -54,31 +112,36 @@ func getAccountInBox(accountListWidget *widget.Box, accounts *dcrlibwallet.Accou
 
 		spendableLabel := canvas.NewText("Spendable", color.White)
 		spendableLabel.TextSize = 10
-		spendableLabel.Alignment = fyne.TextAlignLeading
-
-		spendableAmountLabel := canvas.NewText(dcrutil.Amount(account.Balance.Spendable).String(), color.White)
-		spendableAmountLabel.TextSize = 10
-		spendableAmountLabel.Alignment = fyne.TextAlignTrailing
 
 		accountName := account.Name
 		accountNameLabel := widget.NewLabel(accountName)
 		accountNameLabel.Alignment = fyne.TextAlignLeading
 		accountNameBox := widget.NewVBox(
 			accountNameLabel,
-			spendableLabel,
+			widget.NewHBox(widgets.NewHSpacer(1), spendableLabel),
 		)
 
-		accountBalance := dcrutil.Amount(account.Balance.Total).String()
+		spendableAmountLabel := canvas.NewText(dcrutil.Amount(account.Balance.Spendable).String(), color.White)
+		spendableAmountLabel.TextSize = 10
+		spendableAmountLabel.Alignment = fyne.TextAlignTrailing
+
+		amount := dcrutil.Amount(account.Balance.Total).String()
+		accountBalance := amount
 		accountBalanceLabel := widget.NewLabel(accountBalance)
 		accountBalanceLabel.Alignment = fyne.TextAlignTrailing
+
 		accountBalanceBox := widget.NewVBox(
 			accountBalanceLabel,
 			spendableAmountLabel,
 		)
 
 		checkmarkIcon := widget.NewIcon(theme.ConfirmIcon())
+		var spacing fyne.CanvasObject
 		if index != 0 {
 			checkmarkIcon.Hide()
+			spacing = widgets.NewHSpacer(35)
+		} else {
+			spacing = widgets.NewHSpacer(15)
 		}
 
 		accountsView := widget.NewHBox(
@@ -86,34 +149,80 @@ func getAccountInBox(accountListWidget *widget.Box, accounts *dcrlibwallet.Accou
 			widget.NewIcon(receiveIcon),
 			widgets.NewHSpacer(20),
 			accountNameBox,
-			widgets.NewHSpacer(20),
+			layout.NewSpacer(),
+			widgets.NewHSpacer(30),
 			accountBalanceBox,
 			widgets.NewHSpacer(30),
 			checkmarkIcon,
-			widgets.NewHSpacer(15),
+			spacing,
 		)
 
-		accountListWidget.Append(widgets.NewClickableBox(accountsView, func() {
+		dropdownContent.Append(widgets.NewClickableBox(accountsView, func() {
 			// hide checkmark icon of other accounts
-			for _, children := range accountListWidget.Children {
+			for _, children := range dropdownContent.Children {
 				if box, ok := children.(*widgets.ClickableBox); !ok {
 					continue
 				} else {
-					if len(box.Children) != 9 {
+					if len(box.Children) != 10 {
 						continue
 					}
 
-					if icon, ok := box.Children[7].(*widget.Icon); !ok {
+					if icon, ok := box.Children[8].(*widget.Icon); !ok {
 						continue
 					} else {
 						icon.Hide()
 					}
+					if spacing, ok := box.Children[9].(*fyne.Container); !ok {
+						continue
+					} else {
+						spacing.Layout = layout.NewFixedGridLayout(fyne.NewSize(35, 0))
+						canvas.Refresh(spacing)
+					}
 				}
+				canvas.Refresh(children)
 			}
 
 			checkmarkIcon.Show()
-			// what happens after selecting
+			if spacing, ok := accountsView.Children[9].(*fyne.Container); !ok {
+			} else {
+				spacing.Layout = layout.NewFixedGridLayout(fyne.NewSize(15, 0))
+				canvas.Refresh(spacing)
+			}
+
+			if accountbalanceBox, ok := accountsView.Children[6].(*widget.Box); ok {
+				if len(accountbalanceBox.Children) == 2 {
+					if balanceLabel, ok := accountbalanceBox.Children[0].(*widget.Label); ok {
+						selectedAccountBalanceLabel.SetText(balanceLabel.Text)
+					}
+				}
+			}
+			selectedAccountLabel.SetText(accountName)
 			popup.Hide()
 		}))
+	}
+}
+
+func updateAccountBoxContent(accountBox *widget.Box, account *dcrlibwallet.Accounts) {
+	for index, boxContent := range accountBox.Children {
+		spendableAmountLabel := canvas.NewText(dcrutil.Amount(account.Acc[index].Balance.Spendable).String(), color.White)
+		spendableAmountLabel.TextSize = 10
+		spendableAmountLabel.Alignment = fyne.TextAlignTrailing
+
+		accountBalance := dcrutil.Amount(account.Acc[index].Balance.Total).String()
+		accountBalanceLabel := widget.NewLabel(accountBalance)
+		accountBalanceLabel.Alignment = fyne.TextAlignTrailing
+
+		accountBalanceBox := widget.NewVBox(
+			accountBalanceLabel,
+			spendableAmountLabel,
+		)
+
+		accountBalance = dcrutil.Amount(account.Acc[index].Balance.Total).String()
+
+		if content, ok := boxContent.(*widgets.ClickableBox); ok {
+			content.Box.Children[6] = accountBalanceBox
+			widget.Refresh(content.Box)
+			widget.Refresh(content)
+		}
 	}
 }
--- fyne/pages/txnotifier.go
+++ fyne/pages/txnotifier.go
@@ -1,106 +1,53 @@
 package pages
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"strconv"
 
 	"fyne.io/fyne/widget"
-
-	"github.com/gen2brain/beeep"
+	"github.com/decred/dcrd/dcrutil"
 	"github.com/raedahgroup/dcrlibwallet"
 )
 
-type multiWalletTxListener struct {
-	tabMenu     *widget.TabContainer
-	multiWallet *dcrlibwallet.MultiWallet
+type listener struct {
+	tabMenu *widget.TabContainer
+	dcrlw   *dcrlibwallet.LibWallet
+	// Place widgets that should be dynamically updated on fyne.
+	// Only update currently viewed tab and only update widget children.
 }
 
-func (app *multiWalletTxListener) OnSyncStarted() {
-
-}
-
-func (app *multiWalletTxListener) OnPeerConnectedOrDisconnected(numberOfConnectedPeers int32) {
-
-}
-
-func (app *multiWalletTxListener) OnHeadersFetchProgress(headersFetchProgress *dcrlibwallet.HeadersFetchProgressReport) {
-
-}
-
-func (app *multiWalletTxListener) OnAddressDiscoveryProgress(addressDiscoveryProgress *dcrlibwallet.AddressDiscoveryProgressReport) {
-
-}
-
-func (app *multiWalletTxListener) OnHeadersRescanProgress(headersRescanProgress *dcrlibwallet.HeadersRescanProgressReport) {
-
-}
-
-func (app *multiWalletTxListener) OnSyncCompleted() {
-
-}
-
-func (app *multiWalletTxListener) OnSyncCanceled(willRestart bool) {
-
-}
-
-func (app *multiWalletTxListener) OnSyncEndedWithError(err error) {
-
-}
-
-func (app *multiWalletTxListener) Debug(debugInfo *dcrlibwallet.DebugInfo) {
-
-}
-
-func (app *multiWalletTxListener) OnTransaction(transaction string) {
-	var currentTransaction dcrlibwallet.Transaction
-	err := json.Unmarshal([]byte(transaction), &currentTransaction)
-	if err != nil {
-		log.Println("could not read transaction to json")
-		return
-	}
-
-	amount := dcrlibwallet.AmountCoin(currentTransaction.Amount)
-	var notification string
-
-	if app.multiWallet.OpenedWalletsCount() > 1 {
-		wallet := app.multiWallet.WalletWithID(currentTransaction.WalletID)
-		if wallet == nil {
+func (test *listener) OnTransaction(transaction string) {
+	if test.tabMenu.CurrentTabIndex() == 0 {
+		// place overview page dynamic data here
+	} else if test.tabMenu.CurrentTabIndex() == 2 { // place send page dynamic data here
+		accountNumber, err := test.dcrlw.AccountNumber(sendPage.receivingSelectedAccountLabel.Text)
+		if err != nil {
 			return
 		}
-		notification = fmt.Sprintf("[%s] You have received %s DCR", wallet.Name, strconv.FormatFloat(amount, 'f', -1, 64)) // remove trailing zeros
-	} else {
-		notification = fmt.Sprintf("You have received %s DCR", strconv.FormatFloat(amount, 'f', -1, 64))
-	}
+		balance, err := test.dcrlw.GetAccountBalance(int32(accountNumber), dcrlibwallet.DefaultRequiredConfirmations)
+		sendPage.receivingSelectedAccountBalanceLabel.SetText(dcrutil.Amount(balance.Total).String())
 
-	err = beeep.Notify("Decred Fyne Wallet", notification, "assets/information.png")
-	if err != nil {
-		log.Println("could not initiate desktop notification, reason:", err.Error())
-	}
-
-	// place all dynamic widgets here to be updated only when tabmenu is in view.
-	if app.tabMenu.CurrentTabIndex() == 0 {
-
-	} else if app.tabMenu.CurrentTabIndex() == 2 {
-
-	} else if app.tabMenu.CurrentTabIndex() == 3 {
-
-	} else if app.tabMenu.CurrentTabIndex() == 2 {
-
+		accounts, _ := test.dcrlw.GetAccountsRaw(dcrlibwallet.DefaultRequiredConfirmations)
+		updateAccountBoxContent(sendPage.receivingAccount, accounts)
+	} else if test.tabMenu.CurrentTabIndex() == 3 {
+		// place receive page dynamic data here
+	} else if test.tabMenu.CurrentTabIndex() == 2 {
+		// place account page dynamic data here
 	}
 }
 
-func (app *multiWalletTxListener) OnTransactionConfirmed(walletID int, hash string, blockHeight int32) {
-	// place all dynamic widgets in a function here, to be updated only when tabmenu is in view.
-	if app.tabMenu.CurrentTabIndex() == 0 {
+func (test *listener) OnTransactionConfirmed(hash string, height int32) {
+	fmt.Println("working OnTransactionConfirmed", hash, height)
+	if test.tabMenu.CurrentTabIndex() == 0 {
+		// place overview page dynamic data here
+	} else if test.tabMenu.CurrentTabIndex() == 2 {
+		// place send page dynamic data here
 
-	} else if app.tabMenu.CurrentTabIndex() == 2 {
-
-	} else if app.tabMenu.CurrentTabIndex() == 3 {
-
-	} else if app.tabMenu.CurrentTabIndex() == 2 {
-
+		accounts, _ := test.dcrlw.GetAccountsRaw(dcrlibwallet.DefaultRequiredConfirmations)
+		updateAccountBoxContent(sendPage.receivingAccount, accounts)
+	} else if test.tabMenu.CurrentTabIndex() == 3 {
+		// place receive page dynamic data here
+	} else if test.tabMenu.CurrentTabIndex() == 2 {
+		// place account page dynamic data here
 	}
 }
 
@@ -117,13 +64,9 @@ func (app *multiWalletTxListener) OnBlockAttached(walletID int, blockHeight int3
 	}
 }
 
-func (app *AppInterface) walletNotificationListener() {
-	var dcrListener multiWalletTxListener
-	dcrListener.tabMenu = app.tabMenu
-	dcrListener.multiWallet = app.MultiWallet
-
-	err := app.MultiWallet.AddSyncProgressListener(&dcrListener, "")
-	if err != nil {
-		log.Fatalln("could not start progress multiWalletTxListener")
-	}
+func (app *AppInterface) walletNotificationListener(dcrlistener *listener) {
+	// pass dynamic variables.
+	dcrlistener.tabMenu = app.tabMenu
+	dcrlistener.dcrlw = app.Dcrlw
+	app.Dcrlw.TransactionNotification(dcrlistener)
 }
