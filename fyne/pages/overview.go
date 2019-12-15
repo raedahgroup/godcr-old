306e7b944ffed9e3848e5befa455a9982afc01e7
metaclips <utimichael9@gmail.com>
2019-12-15 15:58:54 +0100

    fixed padding to overview

--- fyne/pages/overview.go
+++ fyne/pages/overview.go
@@ -1,20 +1,26 @@
 package pages
 
 import (
+	"fmt"
 	"image/color"
+	"sort"
+	"strings"
+	"time"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 
+	"github.com/decred/dcrd/dcrutil"
 	"github.com/raedahgroup/dcrlibwallet"
 	"github.com/raedahgroup/godcr/fyne/assets"
+	"github.com/raedahgroup/godcr/fyne/pages/handler/values"
 	"github.com/raedahgroup/godcr/fyne/widgets"
 )
 
 const historyPageIndex = 1
-const PageTitle = "overview"
+const PageTitle = "Overview"
 
 type overview struct {
 	app            *AppInterface
@@ -27,26 +33,42 @@ type overview struct {
 // todo: display overview page (include sync progress UI elements)
 // todo: register sync progress listener on overview page to update sync progress views
 func overviewPageContent(app *AppInterface) fyne.CanvasObject {
-	return widget.NewHBox(widgets.NewHSpacer(18), container())
+	ov := &overview{}
+	ov.app = app
+	// app.Window.Resize(fyne.NewSize(650, 650))
+	ov.multiWallet = app.MultiWallet
+	ov.walletIds = ov.multiWallet.OpenedWalletIDsRaw()
+	if len(ov.walletIds) == 0 {
+		return widget.NewHBox(widgets.NewHSpacer(10), widget.NewLabelWithStyle("Could not retrieve wallets", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}))
+	}
+	sort.Ints(ov.walletIds)
+	return widget.NewHBox(widgets.NewHSpacer(values.Padding), ov.container(), widgets.NewHSpacer(values.Padding))
 }
 
-func container() fyne.CanvasObject {
+func (ov *overview) container() fyne.CanvasObject {
 	return widget.NewVBox(
+		widgets.NewVSpacer(values.Padding),
 		title(),
 		ov.balance(),
 		widgets.NewVSpacer(50),
-		pageBoxes(),
+		ov.pageBoxes(),
+		widgets.NewVSpacer(values.Padding),
 	)
 }
 
 func title() fyne.CanvasObject {
-	titleWidget := widget.NewLabelWithStyle(PageTitle, fyne.TextAlignLeading, fyne.TextStyle{Bold: true, Italic: true})
+	titleWidget := widget.NewLabelWithStyle(PageTitle, fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
 	return widget.NewHBox(titleWidget)
 }
 
-func balance() fyne.CanvasObject {
-	dcrBalance := widgets.NewLargeText("315.08", color.Black)
-	dcrDecimals := widgets.NewSmallText("193725 DCR", color.Black)
+func (ov *overview) balance() fyne.CanvasObject {
+	tb, err := totalBalance(ov)
+	if err != nil {
+		return widget.NewLabel(fmt.Sprintf("Error: %s", err.Error()))
+	}
+	mainBalance, subBalance := breakBalance(tb)
+	dcrBalance := widgets.NewLargeText(mainBalance, color.Black)
+	dcrDecimals := widgets.NewSmallText(subBalance, color.Black)
 	decimalsBox := fyne.NewContainerWithLayout(layout.NewVBoxLayout(), widgets.NewVSpacer(6), dcrDecimals)
 	return widget.NewHBox(widgets.NewVSpacer(10), dcrBalance, decimalsBox)
 }
@@ -55,25 +77,27 @@ func (ov *overview) pageBoxes() (object fyne.CanvasObject) {
 	return fyne.NewContainerWithLayout(layout.NewVBoxLayout(),
 		blockStatusBox(),
 		widgets.NewVSpacer(15),
-		recentTransactionBox(),
+		ov.recentTransactionBox(),
 	)
 }
 
-func recentTransactionBox() fyne.CanvasObject {
+func (ov *overview) recentTransactionBox() fyne.CanvasObject {
+	var err error
+	ov.transactions, err = recentTransactions(ov)
+	if err != nil {
+		return widget.NewHBox(widgets.NewHSpacer(10), widget.NewLabelWithStyle(err.Error(), fyne.TextAlignCenter, fyne.TextStyle{Bold: true}))
+	}
+
 	table := &widgets.Table{}
-	// add a maximum of 5 rows to the recent transaction box
-	table.NewTable(transactionRowHeader(),
-		newTransactionRow(assets.ReceiveIcon, "0.0000004 DCR", "0.0000004 DCR",
-			"yourself", "confirmed", "08-11-2019"),
-		newTransactionRow(assets.SendIcon, "0.0000004 DCR",
-			"0.0000004 DCR", "yourself", "confirmed", "08-11-2019"),
-		newTransactionRow(assets.ReceiveIcon, "0.0000004 DCR",
-			"0.0000004 DCR", "yourself", "confirmed", "08-11-2019"),
-		newTransactionRow(assets.SendIcon, "0.0000004 DCR",
-			"0.0000004 DCR", "yourself", "confirmed", "08-11-2019"),
-		newTransactionRow(assets.SendIcon, "0.0000004 DCR",
-			"0.0000004 DCR", "yourself", "confirmed", "08-11-2019"),
-	)
+	table.NewTable(transactionRowHeader())
+	for _, txn := range ov.transactions {
+		amount := dcrutil.Amount(txn.Amount).String()
+		fee := dcrutil.Amount(txn.Fee).String()
+		timeDate := dcrlibwallet.ExtractDateOrTime(txn.Timestamp)
+		status := transactionStatus(ov, txn)
+		table.Append(newTransactionRow(transactionIcon(txn.Direction), amount, fee,
+			dcrlibwallet.TransactionDirectionName(txn.Direction), status, timeDate))
+	}
 
 	return widget.NewVBox(
 		table.Result,
@@ -82,7 +106,7 @@ func recentTransactionBox() fyne.CanvasObject {
 			widgets.NewClickableBox(
 				widget.NewHBox(widget.NewLabelWithStyle("see all", fyne.TextAlignCenter, fyne.TextStyle{Italic: true})),
 				func() {
-					// todo
+					ov.app.tabMenu.SelectTabIndex(historyPageIndex)
 				},
 			),
 			layout.NewSpacer(),
@@ -189,4 +213,74 @@ func transactionRowHeader() *widget.Box {
 	status := widget.NewLabelWithStyle("status", fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
 	date := widget.NewLabelWithStyle("date", fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
 	return widget.NewHBox(hash, amount, fee, direction, status, date)
+}
+
+func totalBalance(overview *overview) (balance string, err error) {
+	var totalWalletBalance int64
+	mw := overview.multiWallet
+	for _, id := range overview.walletIds {
+		accounts, err := mw.WalletWithID(id).GetAccountsRaw(dcrlibwallet.DefaultRequiredConfirmations)
+		if err != nil {
+			return "", err
+		}
+		for _, acc := range accounts.Acc {
+			totalWalletBalance += acc.TotalBalance
+		}
+	}
+	balance = dcrutil.Amount(totalWalletBalance).String()
+	return
+}
+
+func breakBalance(balance string) (b1, b2 string) {
+	balanceParts := strings.Split(balance, ".")
+	b1 = balanceParts[0]
+	if len(balanceParts) > 1 {
+		b2 = balanceParts[1]
+		b1 = b1 + "." + b2[:2]
+		b2 = b2[2:]
+	}
+	return
+}
+
+func recentTransactions(overview *overview) (transactions []dcrlibwallet.Transaction, err error) {
+	mw := overview.multiWallet
+
+	// add recent transactions of all wallets to a single slice
+	for _, id := range overview.walletIds {
+		txns, err := mw.WalletWithID(id).GetTransactionsRaw(0, 10, 0, true)
+		transactions = append(transactions, txns...)
+		if err != nil {
+			return nil, err
+		}
+	}
+	sort.SliceStable(transactions, func(i, j int) bool {
+		backTime := time.Unix(transactions[j].Timestamp, 0)
+		frontTime := time.Unix(transactions[i].Timestamp, 0)
+		return backTime.Before(frontTime)
+	})
+	if len(transactions) > 5 {
+		transactions = transactions[:5]
+	}
+	return
+}
+
+func transactionIcon(direction int32) string {
+	switch direction {
+	case 0:
+		return assets.SendIcon
+	case 1:
+		return assets.ReceiveIcon
+	case 2:
+		return assets.ReceiveIcon
+	default:
+		return assets.InfoIcon
+	}
+}
+
+func transactionStatus(overview *overview, txn dcrlibwallet.Transaction) string {
+	confirmations := overview.multiWallet.GetBestBlock().Height - txn.BlockHeight + 1
+	if txn.BlockHeight != -1 && confirmations > dcrlibwallet.DefaultRequiredConfirmations {
+		return "confirmed"
+	}
+	return "pending"
 }
