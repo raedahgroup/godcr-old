344761007baeb399be113ebad0c5cd9033a26b4f
metaclips <utimichael9@gmail.com>
2019-12-21 00:41:51 +0100

    finished up accountselector box

--- fyne/pages/handler/walletshandler/walletselector.go
+++ fyne/pages/handler/walletshandler/walletselector.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"image/color"
 	"sort"
+	"strconv"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/canvas"
@@ -25,7 +26,7 @@ func (walletPage *WalletPageObject) accountSelector() error {
 }
 
 func (walletPage *WalletPageObject) getAccountsInWallet(expandIcon, accountIcon, importedAccountIcon,
-	addAccountIcon fyne.Resource, selectedWalletID int) {
+	addAccountIcon, moreIcon fyne.Resource, selectedWalletID int) {
 
 	selectedWallet := walletPage.MultiWallet.WalletWithID(selectedWalletID)
 	accts, err := selectedWallet.GetAccountsRaw(dcrlibwallet.DefaultRequiredConfirmations)
@@ -37,10 +38,15 @@ func (walletPage *WalletPageObject) getAccountsInWallet(expandIcon, accountIcon,
 	for _, acc := range accts.Acc {
 		totalBalance += acc.TotalBalance
 	}
+	balanceInString := strconv.FormatFloat(dcrlibwallet.AmountCoin(totalBalance), 'f', 8, 64)
 
 	// walletPage.WalletTotalAmountText[selectedWalletID].Text=selected
 	accountBox := widget.NewHBox(widget.NewIcon(expandIcon), widgets.NewHSpacer(4),
 		widget.NewIcon(accountIcon), widgets.NewHSpacer(12),
-		canvas.NewText(selectedWallet.Name, color.RGBA{9, 20, 64, 255}), widgets.NewHSpacer(50))
+		canvas.NewText(selectedWallet.Name, color.RGBA{9, 20, 64, 255}), widgets.NewHSpacer(50),
+		canvas.NewText(balanceInString, color.RGBA{89, 109, 129, 255}), widgets.NewHSpacer(4),
+		widgets.NewImageButton(moreIcon, nil, func() {
+
+		}), widgets.NewHSpacer(12))
 
 }
